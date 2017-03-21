1313346f4e45b93be433fd05803dcd7aee192dc7
Jeff Martin <jeff@reportmill.com>
2017-03-21 09:40:36 -0500

    Key.getStandard(name): Fixed to make sure first character isn't digit.

--- src/snap/util/Key.java
+++ src/snap/util/Key.java
@@ -208,6 +208,10 @@ public static String getStandard(String aKey)
     if(Character.isLowerCase(name.charAt(0)))
         name = Character.toUpperCase(name.charAt(0)) + name.substring(1);
     
+    // Make sure first character isn't number
+    if(Character.isDigit(name.charAt(0)))
+        name = aKey;
+    
     // Return name
     return name;
 }
