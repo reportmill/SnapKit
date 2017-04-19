dc88df55cb6acddaf55ef90252d21678f5aa9498
Jeff Martin <jeff@reportmill.com>
2017-04-19 09:58:47 -0500

    SpringView: Added code to ResetSpringInfo if child bounds changes when not InLayout.

--- src/snap/view/SpringView.java
+++ src/snap/view/SpringView.java
@@ -12,6 +12,9 @@ public class SpringView extends ChildView {
     // The HBox layout
     ViewLayout.SpringLayout  _layout = new ViewLayout.SpringLayout(this);
     
+    // A PropChangeListener to resetSpringInfo when child bounds change outside of layout
+    PropChangeListener       _pcl = pce -> childPropChange(pce);
+    
 /**
  * Override to add layout info.
  */
@@ -19,6 +22,7 @@ public void addChild(View aChild, int anIndex)
 {
     super.addChild(aChild, anIndex);
     _layout.addSpringInfo(aChild);
+    aChild.addPropChangeListener(_pcl);
 }
 
 /**
@@ -28,24 +32,36 @@ public View removeChild(int anIndex)
 {
     View child = super.removeChild(anIndex);
     _layout.removeSpringInfo(child);
+    child.removePropChangeListener(_pcl);
     return child;
 }
 
 /**
- * Resets spring info for children.
+ * Resets spring info for given child (or all children if null).
  */
-public void resetSpringInfo()
+public void resetSpringInfo(View aChild)
 {
-    for(View v : getChildren()) {
-        _layout.removeSpringInfo(v);
+    if(aChild!=null)
+        _layout.addSpringInfo(aChild);
+    else for(View v : getChildren())
         _layout.addSpringInfo(v);
-    }
 }
 
 /**
  * Layout children.
  */
 protected void layoutImpl()  { _layout.layoutChildren(); }
+
+/**
+ * Called when child property changes.
+ */
+protected void childPropChange(PropChange aPCE)
+{
+    if(isInLayout()) return;
+    String pname = aPCE.getPropertyName();
+    if(pname==X_Prop || pname==Y_Prop || pname==Width_Prop || pname==Height_Prop)
+        resetSpringInfo((View)aPCE.getSource());
+}
 
 /**
  * XML Archival.
