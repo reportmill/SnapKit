89d05c4cad79f83a669e4fec254f489db317d9bb
Jeff Martin <jeff@reportmill.com>
2017-12-04 20:04:13 -0600

    Slight fix.

--- src/snap/view/PopupWindow.java
+++ src/snap/view/PopupWindow.java
@@ -26,7 +26,7 @@ public RootView getRootView()
 {
     if(_rview!=null) return _rview;
     RootView rview = new RootView(); rview.setFill(Color.WHITE); rview.setBorder(Color.LIGHTGRAY,1);
-    setRootView(rview);
+    setRootView(rview); _rview._win = this;
     return _rview;
 }
 
