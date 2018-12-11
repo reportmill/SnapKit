16e08453194b96af1fc7287ac6d2c7e0731cfc9e
Jeff Martin <jeff@reportmill.com>
2018-12-10 20:02:05 -0600

    ClassUtils: Moved some fancy methods to SnapCode JavaKit ClassExtras

--- src/snap/util/ClassUtils.java
+++ src/snap/util/ClassUtils.java
@@ -17,6 +17,29 @@ public class ClassUtils {
         Float.class, Double.class, Void.class };
 
 /**
+ * Returns the given object as instance of given class, if it is.
+ */
+public static <T> T getInstance(Object anObj, Class <T> aClass)  { return aClass.isInstance(anObj)? (T)anObj : null; }
+
+/**
+ * Returns a new instance of a given object.
+ */
+public static <T> T newInstance(T anObject)
+{
+    try { return (T)getClass(anObject).newInstance(); }
+    catch(Exception e) { throw new RuntimeException(e); }
+}
+
+/**
+ * Returns a new instance of a given class.
+ */
+public static <T> T newInstance(Class <T> aClass)
+{
+    try { return aClass.newInstance(); }
+    catch(Exception e) { throw new RuntimeException(e); }
+}
+
+/**
  * Returns the class for an object.
  */
 public static Class getClass(Object anObj)
@@ -58,9 +81,11 @@ public static Class getClass(String aName, ClassLoader aClassLoader)
 /**
  * Class.getMethod wrapper to isolate call to one place.
  */
-public static Method getMethodOrThrow(Object anObj, String aName, Class ... theClasses) throws NoSuchMethodException
+public static Method getMethod(Class aClass, String aName, Class ... theClasses)
 {
-    return getMethodOrThrow(anObj.getClass(), aName, theClasses);
+    try { return getMethodOrThrow(aClass, aName, theClasses); }
+    catch(NoSuchMethodException e) { return null; }
+    catch(SecurityException e) { return null; }
 }
 
 /**
@@ -69,6 +94,68 @@ public static Method getMethodOrThrow(Object anObj, String aName, Class ... theC
 public static Method getMethodOrThrow(Class aClass, String aName, Class ... theClasses) throws NoSuchMethodException
 {
     return aClass.getMethod(aName, theClasses);
+}
+
+/**
+ * Returns the method for given class and name that best matches given parameter types.
+ */
+public static Method getMethodBest(Class aClass, String aName, Class ... theClasses)
+{
+    // Get methods with compatible name/args (just return if null, no args or singleton)
+    Method methods[] = getMethods(aClass, aName, theClasses, null);
+    if(methods==null) return null;
+    if(theClasses.length==0 || methods.length==1) return methods[0];
+    
+    // Rate compatible methods and return the most compatible
+    Method method = null; int rating = 0;
+    for(Method meth : methods) {
+        int rtg = getRating(meth.getParameterTypes(), theClasses, meth.isVarArgs());
+        if(rtg>rating) { method = meth; rating = rtg; }}
+    return method;
+}
+
+/**
+ * Returns the method for given class, name and parameter types.
+ */
+private static Method[] getMethods(Class aClass, String aName, Class theClasses[], Method theResult[])
+{
+    // Make sure class is non-primitive
+    Class cls = aClass.isPrimitive()? fromPrimitive(aClass) : aClass;
+    
+    // Check declared methods
+    Method methods[] = getDeclaredMethods(cls, aName, theClasses, theResult);
+    
+    // If interface, check extended interfaces
+    if(cls.isInterface()) {
+        for(Class cl : cls.getInterfaces())
+            methods = getMethods(cl, aName, theClasses, methods); }
+            
+    // Otherwise, check superclass
+    else if((cls=cls.getSuperclass())!=null)
+        methods = getMethods(cls, aName, theClasses, methods);
+        
+    // Return methods
+    return methods;
+}
+
+/**
+ * Returns compatible declared methods for a given class, name and parameter types array.
+ */
+private static Method[] getDeclaredMethods(Class aClass, String aName, Class theClasses[], Method theResult[])
+{
+    // Get class methods and intern name
+    Method methods[] = aClass.getDeclaredMethods();
+    String name = aName.intern();
+    
+    // Iterate over methods and if compatible, add to results
+    for(Method meth : methods) {
+        if(meth.isSynthetic()) continue; 
+        if(meth.getName()==name && isCompatible(meth.getParameterTypes(), theClasses, meth.isVarArgs())) {
+            theResult = theResult!=null? Arrays.copyOf(theResult, theResult.length+1) : new Method[1];
+            theResult[theResult.length-1] = meth;
+        }
+    }
+    return theResult;
 }
 
 /**
@@ -132,47 +219,6 @@ public static String getClassCoded(String aClassName)
 }
 
 /**
- * Returns non primitive type for primitive.
- */
-public static Class toPrimitive(Class aClass)
-{
-    for(int i=0; i<_primitives.length; i++) if(aClass==_primMappings[i]) return _primitives[i];
-    return aClass;
-}
-
-/**
- * Returns primitive type for non-primitive.
- */
-public static Class fromPrimitive(Class aClass)
-{
-    for(int i=0; i<_primitives.length; i++) if(aClass==_primitives[i]) return _primMappings[i];
-    return aClass;
-}
-
-/**
- * Returns the given object as instance of given class, if it is.
- */
-public static <T> T getInstance(Object anObj, Class <T> aClass)  { return aClass.isInstance(anObj)? (T)anObj : null; }
-
-/**
- * Returns a new instance of a given object.
- */
-public static <T> T newInstance(T anObject)
-{
-    try { return (T)getClass(anObject).newInstance(); }
-    catch(Exception e) { throw new RuntimeException(e); }
-}
-
-/**
- * Returns a new instance of a given class.
- */
-public static <T> T newInstance(Class <T> aClass)
-{
-    try { return aClass.newInstance(); }
-    catch(Exception e) { throw new RuntimeException(e); }
-}
-
-/**
  * Returns whether a given class could be assigned a value from the second given class (accounting for auto-boxing).
  */
 public static boolean isAssignable(Class aClass1, Class aClass2)
@@ -200,7 +246,7 @@ public static boolean isAssignablePrimitive(Class aClass1, Class aClass2)
 /**
  * Returns whether second batch of classes is assignable to first batch of classes (accounting for auto-boxing).
  */
-public static boolean isAssignable(Class theClasses1[], Class theClasses2[], int aCount)
+protected static boolean isAssignable(Class theClasses1[], Class theClasses2[], int aCount)
 {
     if(theClasses1==null) return theClasses2==null || theClasses2.length==0;
     if(theClasses2==null) return theClasses1.length==0;
@@ -213,7 +259,7 @@ public static boolean isAssignable(Class theClasses1[], Class theClasses2[], int
 /**
  * Returns whether arg classes are compatible.
  */
-public static boolean isCompatible(Class params[], Class theClasses[], boolean isVarArgs)
+protected static boolean isCompatible(Class params[], Class theClasses[], boolean isVarArgs)
 {
     // Handle Var args
     if(isVarArgs) {
@@ -245,7 +291,7 @@ public static boolean isCompatible(Class params[], Class theClasses[], boolean i
 /**
  * Returns a rating of a method for given possible arg classes.
  */
-private static int getRating(Class theParamTypes[], Class theClasses[], boolean isVarArgs)
+protected static int getRating(Class theParamTypes[], Class theClasses[], boolean isVarArgs)
 {
     // Iterate over classes and add score based on matching classes
     // This is a punt - need to groc the docs on this: https://docs.oracle.com/javase/specs/jls/se7/html/jls-15.html
@@ -280,233 +326,33 @@ private static int getRating(Class theParamTypes[], Class theClasses[], boolean 
 }
 
 /**
- * Returns a field for a parent class and a name.
+ * Returns non primitive type for primitive.
  */
-public static Field getField(Class aClass, String aName)
+public static Class toPrimitive(Class aClass)
 {
-    Class cls = aClass.isPrimitive()? fromPrimitive(aClass) : aClass;
-    Field field = getDeclaredField(cls, aName);
-    if(field!=null)
-        return field;
-    
-    // Check superclass
-    Class sclass = cls.getSuperclass();
-    if(sclass!=null)
-        field = getField(sclass, aName);
-    if(field!=null)
-        return field;
-    
-    // Check interfaces
-    for(Class c : cls.getInterfaces()) {
-        field = getField(c, aName);
-        if(field!=null)
-            return field; }
-    
-    // Return null since not found
-    return null;
+    for(int i=0; i<_primitives.length; i++) if(aClass==_primMappings[i]) return _primitives[i];
+    return aClass;
 }
 
 /**
- * Returns a field for a parent class and a name.
+ * Returns primitive type for non-primitive.
  */
-public static Field getDeclaredField(Class aClass, String aName)
+public static Class fromPrimitive(Class aClass)
 {
-    Field fields[] = aClass.getDeclaredFields();
-    for(Field field : fields)
-        if(field.getName().equals(aName))
-            return field;
-    return null;
+    for(int i=0; i<_primitives.length; i++) if(aClass==_primitives[i]) return _primMappings[i];
+    return aClass;
 }
 
 /**
- * Returns all methods for given class and subclasses that start with given prefix.
+ * Returns the common ancestor class for a list of objects.
  */
-public static Method[] getMethods(Class aClass, String aPrefix)
+public static Class getCommonClass(List aList)
 {
-    List meths = new ArrayList();
-    getMethods(aClass, aPrefix.toLowerCase(), meths, true);
-    return (Method[])meths.toArray(new Method[0]);
-}
-
-/**
- * Returns the method for given class, name and parameter types.
- */
-private static void getMethods(Class aClass, String aPrefix, List theMethods, boolean doPrivate)
-{
-    Class cls = aClass.isPrimitive()? fromPrimitive(aClass) : aClass;
-    Method methods[] = cls.getDeclaredMethods();
-    for(Method meth : methods) {
-        if(meth.isSynthetic()) continue;
-        if(meth.getName().toLowerCase().startsWith(aPrefix) && (doPrivate || !Modifier.isPrivate(meth.getModifiers())))
-            theMethods.add(meth);
-    }
-    
-    // If interface, recurse for extended interfaces
-    if(cls.isInterface()) {
-        for(Class cl : cls.getInterfaces())
-            getMethods(cl, aPrefix, theMethods, false); }
-            
-    // Otherwise, recurse for extended superclass
-    else if((cls=cls.getSuperclass())!=null)
-        getMethods(cls, aPrefix, theMethods, false);
-}
-
-/**
- * Returns the method for given class, name and parameter types.
- */
-public static Constructor getConstructor(Class aClass, Class theClasses[])
-{
-    // Get methods that match name/args (just return if null, no args or singleton)
-    if(aClass.isInterface()) return null;
-    Constructor methods[] = getConstructors(aClass, theClasses, null);
-    if(methods==null) return null;
-    if(theClasses.length==0 || methods.length==1) return methods[0];
-    
-    // Rate compatible constructors and return the most compatible
-    Constructor method = null; int rating = 0;
-    for(Constructor meth : methods) {
-        int rtg = getRating(meth.getParameterTypes(), theClasses, meth.isVarArgs());
-        if(rtg>rating) { method = meth; rating = rtg; }}
-    return method;
-}
-
-/**
- * Returns the method for given class, name and parameter types.
- */
-private static Constructor[] getConstructors(Class aClass, Class theClasses[], Constructor theResult[])
-{
-    Class cls = aClass.isPrimitive()? fromPrimitive(aClass) : aClass;
-    Constructor methods[] = getDeclaredConstructors(cls, theClasses, theResult);
-    if((cls=cls.getSuperclass())!=null)
-        methods = getConstructors(cls, theClasses, methods);
-    return methods;
-}
-
-/**
- * Returns the declared method for a given class, name and parameter types array.
- */
-private static Constructor[] getDeclaredConstructors(Class aClass, Class theClasses[], Constructor theResult[])
-{
-    // Iterate over declared constructors and if compatible, add to results
-    Constructor methods[] = aClass.getDeclaredConstructors();
-    for(Constructor meth : methods)
-        if(isCompatible(meth.getParameterTypes(), theClasses, meth.isVarArgs())) {
-            theResult = theResult!=null? Arrays.copyOf(theResult, theResult.length+1) : new Constructor[1];
-            theResult[theResult.length-1] = meth; }
-    return theResult;
-}
-
-/**
- * Returns a method for given name and classes.
- */
-public static Method getMethod(Object anObj, String aName, Class ... theClasses)
-{
-    Class cls = anObj instanceof Class? (Class)anObj : anObj.getClass(); if(cls==null) return null;
-    try { return getMethod(cls, aName, theClasses); }
-    catch(NoSuchMethodError e) { return null; }
-}
-
-/**
- * Returns the method for given class, name and parameter types.
- */
-public static Method getMethod(Class aClass, String aName, Class ... theClasses)
-{
-    // Get methods with compatible name/args (just return if null, no args or singleton)
-    Method methods[] = getMethods(aClass, aName, theClasses, null);
-    if(methods==null) return null;
-    if(theClasses.length==0 || methods.length==1) return methods[0];
-    
-    // Rate compatible methods and return the most compatible
-    Method method = null; int rating = 0;
-    for(Method meth : methods) {
-        int rtg = getRating(meth.getParameterTypes(), theClasses, meth.isVarArgs());
-        if(rtg>rating) { method = meth; rating = rtg; }}
-    return method;
-}
-
-/**
- * Returns the method for given class, name and parameter types.
- */
-private static Method[] getMethods(Class aClass, String aName, Class theClasses[], Method theResult[])
-{
-    // Make sure class is non-primitive
-    Class cls = aClass.isPrimitive()? fromPrimitive(aClass) : aClass;
-    
-    // Check declared methods
-    Method methods[] = getDeclaredMethods(cls, aName, theClasses, theResult);
-    
-    // If interface, check extended interfaces
-    if(cls.isInterface()) {
-        for(Class cl : cls.getInterfaces())
-            methods = getMethods(cl, aName, theClasses, methods); }
-            
-    // Otherwise, check superclass
-    else if((cls=cls.getSuperclass())!=null)
-        methods = getMethods(cls, aName, theClasses, methods);
-        
-    // Return methods
-    return methods;
-}
-
-/**
- * Returns compatible declared methods for a given class, name and parameter types array.
- */
-private static Method[] getDeclaredMethods(Class aClass, String aName, Class theClasses[], Method theResult[])
-{
-    // Get class methods and intern name
-    Method methods[] = aClass.getDeclaredMethods();
-    String name = aName.intern();
-    
-    // Iterate over methods and if compatible, add to results
-    for(Method meth : methods) {
-        if(meth.isSynthetic()) continue; 
-        if(meth.getName()==name && isCompatible(meth.getParameterTypes(), theClasses, meth.isVarArgs())) {
-            theResult = theResult!=null? Arrays.copyOf(theResult, theResult.length+1) : new Method[1];
-            theResult[theResult.length-1] = meth;
-        }
-    }
-    return theResult;
-}
-
-/**
- * Returns a declared inner class for a given class and a name, checking super classes as well.
- */
-public static Class getClass(Class aClass, String aName)
-{
-    // Make sure class is non-primitive
-    Class cls = aClass.isPrimitive()? fromPrimitive(aClass) : aClass;
-    
-    // Check declared inner classes
-    Class cls2 = getDeclaredClass(cls, aName);
-    if(cls2!=null)
-        return cls2;
-    
-    // Check superclass
-    Class sclass = cls.getSuperclass();
-    if(sclass!=null)
-        cls2 = getClass(sclass, aName);
-    if(cls2!=null)
-        return cls2;
-        
-    // Check interfaces
-    for(Class c : cls.getInterfaces()) {
-        cls2 = getClass(c, aName);
-        if(cls2!=null)
-            return cls2; }
-            
-    // Return null since class not found
-    return null;
-}
-
-/**
- * Returns a class for a parent class and a name.
- */
-public static Class getDeclaredClass(Class aClass, String aName)
-{
-    for(Class cls : aClass.getDeclaredClasses())
-        if(cls.getSimpleName().equals(aName))
-            return cls;
-    return null;
+    // Get class for first object, iterate over remaining classes and return common class
+    Class cclass = aList.size()>0? getClass(aList.get(0)) : null;
+    for(int i=1, iMax=aList.size(); i<iMax; i++)
+        cclass = getCommonClass(cclass, aList.get(i));
+    return cclass;
 }
 
 /**
@@ -527,18 +373,6 @@ public static Class getCommonClass(Object anObj1, Object anObj2)
     
     // Recurse by swapping args and using superclass of second
     return getCommonClass(c2.getSuperclass(), c1);
-}
-
-/**
- * Returns the common ancestor class for a list of objects.
- */
-public static Class getCommonClass(List aList)
-{
-    // Get class for first object, iterate over remaining classes and return common class
-    Class cclass = aList.size()>0? getClass(aList.get(0)) : null;
-    for(int i=1, iMax=aList.size(); i<iMax; i++)
-        cclass = getCommonClass(cclass, aList.get(i));
-    return cclass;
 }
 
 }
