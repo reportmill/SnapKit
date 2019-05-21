73c7316e92bde9daae3c057d1d23a9bd58c423fa
Jeff Martin <jeff@reportmill.com>
2019-05-21 16:41:07 -0500

    ViewPhysics: Added Joint and Density attributes

--- src/snap/view/ViewPhysics.java
+++ src/snap/view/ViewPhysics.java
@@ -8,6 +8,15 @@ public class ViewPhysics <T> {
     // Whether view is subject to forces in simulation
     boolean        _dynamic;
     
+    // Whether view is joint
+    boolean        _joint;
+    
+    // The density
+    double         _density = 1;
+    
+    // A group index
+    int            _groupIndex;
+    
     // An ivar to hold a native object for simulation library (Body for Box2D)
     T              _ntv;
 
@@ -20,6 +29,36 @@ public boolean isDynamic()  { return _dynamic; }
  * Sets whether view is subject to forces in simulation.
  */
 public void setDynamic(boolean aValue)  { _dynamic = aValue; }
+
+/**
+ * Returns whether view is a joint.
+ */
+public boolean isJoint()  { return _joint; }
+
+/**
+ * Sets whether view is a joint.
+ */
+public void setJoint(boolean aValue)  { _joint = aValue; }
+
+/**
+ * Returns the body density.
+ */
+public double getDensity()  { return _density; }
+
+/**
+ * Sets the body density.
+ */
+public void setDensity(double aValue)  { _density = aValue; }
+
+/**
+ * Returns the group index.
+ */
+public int getGroupIndex()  { return _groupIndex; }
+
+/**
+ * Sets the group index.
+ */
+public void setGroupIndex(int aValue)  { _groupIndex = aValue; }
 
 /**
  * Returns the native object.
