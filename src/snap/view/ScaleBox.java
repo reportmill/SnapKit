584bc0cfb2f3ac27d3b4872dd5fee123603a2cd5
Jeff Martin <jeff@reportmill.com>
2020-09-10 13:04:56 -0500

    Fix for NPE.

--- src/snap/view/ScaleBox.java
+++ src/snap/view/ScaleBox.java
@@ -11,94 +11,104 @@ import snap.geom.Size;
  */
 public class ScaleBox extends BoxView {
 
-/**
- * Creates a new ScaleBox.
- */
-public ScaleBox()  { }
+    /**
+     * Creates a new ScaleBox.
+     */
+    public ScaleBox()  { }
 
-/**
- * Creates a new ScaleBox for content.
- */
-public ScaleBox(View aContent)  { this(); setContent(aContent); }
+    /**
+     * Creates a new ScaleBox for content.
+     */
+    public ScaleBox(View aContent)  { this(); setContent(aContent); }
 
-/**
- * Creates a new ScaleBox for content with FillWidth, FillHeight params.
- */
-public ScaleBox(View aContent, boolean isFillWidth, boolean isFillHeight)
-{
-    this(aContent); setFillWidth(isFillWidth); setFillHeight(isFillHeight);
-}
-
-/**
- * Returns the aspect of the content.
- */
-protected double getAspect()
-{
-    View c = getContent();
-    Size bs = c.getBestSize();
-    return c.isHorizontal()? bs.width/bs.height : bs.height/bs.width;
-}
-    
-/**
- * Returns preferred width of layout.
- */
-protected double getPrefWidthImpl(double aH)
-{
-    // If scaling and value provided, return value by aspect
-    if(aH>=0 && (isFillHeight() || aH<getPrefHeight(-1))) return aH*getAspect();
-    return super.getPrefWidthImpl(aH);
-}
-
-/**
- * Returns preferred height of layout.
- */
-protected double getPrefHeightImpl(double aW)
-{
-    // If scaling and value provided, return value by aspect
-    if(aW>=0 && (isFillWidth() || aW<getPrefWidth(-1))) return aW/getAspect();
-    return super.getPrefHeightImpl(aW);
-}
-
-/**
- * Performs layout.
- */
-protected void layoutImpl()  { layout(this, getContent(), null, isFillWidth(), isFillHeight()); }
-    
-/**
- * Performs Box layout for given parent, child and fill width/height.
- */
-public static void layout(ParentView aPar, View aChild, Insets theIns, boolean isFillWidth, boolean isFillHeight)
-{
-    // If no child, just return
-    if(aChild==null) return;
-    
-    // Get parent bounds for insets (just return if empty)
-    Insets ins = theIns!=null? theIns : aPar.getInsetsAll();
-    double px = ins.left, py = ins.top;
-    double pw = aPar.getWidth() - px - ins.right; if(pw<0) pw = 0; if(pw<=0) return;
-    double ph = aPar.getHeight() - py - ins.bottom; if(ph<0) ph = 0; if(ph<=0) return;
-    
-    // Get content width/height
-    double cw = aChild.getBestWidth(-1);
-    double ch = aChild.getBestHeight(cw);
-    
-    // Handle ScaleToFit: Set content bounds centered, calculate scale and set
-    if(isFillWidth || isFillHeight || cw>pw || ch>ph)  {
-        double cx = px + (pw-cw)/2, cy = py + (ph-ch)/2;
-        aChild.setBounds(cx, cy, cw, ch);
-        double sx = isFillWidth || cw>pw? pw/cw : 1;
-        double sy = isFillHeight || ch>ph? ph/ch : 1;
-        if(isFillWidth && isFillHeight) sx = sy = Math.min(sx,sy); // KeepAspect?
-        aChild.setScaleX(sx); aChild.setScaleY(sy);
-        return;
+    /**
+     * Creates a new ScaleBox for content with FillWidth, FillHeight params.
+     */
+    public ScaleBox(View aContent, boolean isFillWidth, boolean isFillHeight)
+    {
+        this(aContent);
+        setFillWidth(isFillWidth);
+        setFillHeight(isFillHeight);
     }
-    
-    // Handle normal layout
-    if(cw>pw) cw = pw; if(ch>ph) ch = ph;
-    double dx = pw - cw, dy = ph - ch;
-    double sx = aChild.getLeanX()!=null? ViewUtils.getLeanX(aChild) : ViewUtils.getAlignX(aPar);
-    double sy = aChild.getLeanY()!=null? ViewUtils.getLeanY(aChild) : ViewUtils.getAlignY(aPar);
-    aChild.setBounds(px+dx*sx, py+dy*sy, cw, ch);
-}
-    
+
+    /**
+     * Returns the aspect of the content.
+     */
+    protected double getAspect()
+    {
+        View child = getContent(); if (child==null) return 1;
+        Size bestSize = child.getBestSize();
+        return child.isHorizontal() ? bestSize.width/bestSize.height : bestSize.height/bestSize.width;
+    }
+
+    /**
+     * Returns preferred width of layout.
+     */
+    protected double getPrefWidthImpl(double aH)
+    {
+        // If scaling and value provided, return value by aspect
+        if (aH>=0 && (isFillHeight() || aH<getPrefHeight(-1)))
+            return aH*getAspect();
+        return super.getPrefWidthImpl(aH);
+    }
+
+    /**
+     * Returns preferred height of layout.
+     */
+    protected double getPrefHeightImpl(double aW)
+    {
+        // If scaling and value provided, return value by aspect
+        if (aW>=0 && (isFillWidth() || aW<getPrefWidth(-1)))
+            return aW/getAspect();
+        return super.getPrefHeightImpl(aW);
+    }
+
+    /**
+     * Performs layout.
+     */
+    protected void layoutImpl()
+    {
+        layout(this, getContent(), null, isFillWidth(), isFillHeight());
+    }
+
+    /**
+     * Performs Box layout for given parent, child and fill width/height.
+     */
+    public static void layout(ParentView aPar, View aChild, Insets theIns, boolean isFillWidth, boolean isFillHeight)
+    {
+        // If no child, just return
+        if (aChild==null) return;
+
+        // Get parent bounds for insets (just return if empty)
+        Insets ins = theIns!=null ? theIns : aPar.getInsetsAll();
+        double areaX = ins.left;
+        double areaY = ins.top;
+        double areaW = aPar.getWidth() - areaX - ins.right; if (areaW<0) areaW = 0; if (areaW<=0) return;
+        double areaH = aPar.getHeight() - areaY - ins.bottom; if (areaH<0) areaH = 0; if (areaH<=0) return;
+
+        // Get content width/height
+        double childW = aChild.getBestWidth(-1);
+        double childH = aChild.getBestHeight(childW);
+
+        // Handle ScaleToFit: Set content bounds centered, calculate scale and set
+        if (isFillWidth || isFillHeight || childW>areaW || childH>areaH)  {
+            double childX = areaX + (areaW-childW)/2;
+            double childY = areaY + (areaH-childH)/2;
+            aChild.setBounds(childX, childY, childW, childH);
+            double scaleX = isFillWidth || childW>areaW ? areaW/childW : 1;
+            double scaleY = isFillHeight || childH>areaH ? areaH/childH : 1;
+            if (isFillWidth && isFillHeight)
+                scaleX = scaleY = Math.min(scaleX,scaleY); // KeepAspect?
+            aChild.setScaleX(scaleX);
+            aChild.setScaleY(scaleY);
+            return;
+        }
+
+        // Handle normal layout
+        double dx = areaW - childW;
+        double dy = areaH - childH;
+        double sx = aChild.getLeanX()!=null ? ViewUtils.getLeanX(aChild) : ViewUtils.getAlignX(aPar);
+        double sy = aChild.getLeanY()!=null ? ViewUtils.getLeanY(aChild) : ViewUtils.getAlignY(aPar);
+        aChild.setBounds(areaX+dx*sx, areaY+dy*sy, childW, childH);
+    }
 }
