ab0b9cb15db6571717d91f23de18b319904acd70
Jeff Martin <jeff@reportmill.com>
2020-10-24 15:11:01 -0500

    Some improvements.

--- src/snap/geom/SegPoints.java
+++ src/snap/geom/SegPoints.java
@@ -29,6 +29,12 @@ public class SegPoints extends Shape {
     // The next path (if segs added after close)
     private SegPoints  _nextPath;
 
+    // The total arc length of path
+    private double  _arcLength;
+
+    // The arc lengths for each seg
+    private double  _arcLengths[];
+
     // The winding - how a path determines what to fill when segments intersect
     private int  _wind = WIND_EVEN_ODD;
 
@@ -312,6 +318,127 @@ public class SegPoints extends Shape {
             _nextPath = new SegPoints();
         shapeChanged();
         return _nextPath;
+    }
+
+    /**
+     * Returns the segment at index.
+     */
+    public Segment getSegment(int anIndex)
+    {
+        // Get Seg and Seg PointIndex
+        Seg seg = getSeg(anIndex);
+        int pointIndex = getSegPointIndex(anIndex);
+
+        double p0x = _points[pointIndex];
+        double p0y = _points[pointIndex+1];
+
+        // Handle Line
+        if (seg==Seg.LineTo) {
+            double p1x = _points[pointIndex+2];
+            double p1y = _points[pointIndex+3];
+            return new Line(p0x, p0y, p1x, p1y);
+        }
+
+        // Handle QuadTo
+        else if (seg==Seg.QuadTo) {
+            double cpx = _points[pointIndex+2];
+            double cpy = _points[pointIndex+3];
+            double p1x = _points[pointIndex+4];
+            double p1y = _points[pointIndex+5];
+            return new Quad(p0x, p0y, cpx, cpy, p1x, p1y);
+        }
+
+        // Handle CubicTo
+        else if (seg==Seg.CubicTo) {
+            double cp0x = _points[pointIndex+2];
+            double cp0y = _points[pointIndex+3];
+            double cp1x = _points[pointIndex+4];
+            double cp1y = _points[pointIndex+5];
+            double p1x = _points[pointIndex+6];
+            double p1y = _points[pointIndex+7];
+            return new Cubic(p0x, p0y, cp0x, cp0y, cp1x, cp1y, p1x, p1y);
+        }
+
+        // Handle Close special: PointIndex is to index in ClosePointIndexes (an array of each close {start,end} index)
+        else if (seg==Seg.Close) {
+            double p1x = _points[0];
+            double p1y = _points[1];
+            return new Line(p0x, p0y, p1x, p1y);
+        }
+
+        // No segement
+        return null;
+    }
+
+    /**
+     * Returns the total arc length of path.
+     */
+    public double getArcLength()
+    {
+        if (_arcLengths!=null) return _arcLength;
+        getArcLengths();
+        return _arcLength;
+    }
+
+    /**
+     * Returns the total arc length of path.
+     */
+    public double getSegArcLength(int anIndex)
+    {
+        double arcLens[] = getArcLengths();
+        return arcLens[anIndex];
+    }
+
+    /**
+     * Returns the lengths array.
+     */
+    public double[] getArcLengths()
+    {
+        // If already set, just return
+        if (_arcLengths!=null) return _arcLengths;
+
+        // Get iter vars
+        int segCount = getSegCount();
+        double arcLengths[] = new double[segCount];
+        double pnts[] = new double[8];
+        double arcLength = 0;
+
+        // Iterate over segs and calc lengths
+        for (int i=0; i<segCount; i++) {
+
+            Seg seg = getSegAndPointsForIndex(i, pnts);
+            double len = 0;
+
+            // Get arcLength for seg
+            switch (seg) {
+                case MoveTo: break;
+                case LineTo:
+                    len = Point.getDistance(pnts[0], pnts[1], pnts[2], pnts[3]);
+                    break;
+                case QuadTo:
+                    len = SegmentLengths.getArcLengthQuad(pnts[0], pnts[1], pnts[2], pnts[3], pnts[4], pnts[5]);
+                    break;
+                case CubicTo:
+                    len = SegmentLengths.getArcLengthCubic(pnts[0], pnts[1], pnts[2], pnts[3], pnts[4], pnts[5], pnts[6], pnts[7]);
+                    break;
+                default: throw new RuntimeException("SegPoints.getArcLengths: Unsuppored seg: " + seg);
+            }
+
+            // Update arcLengths
+            arcLengths[i] = len;
+            arcLength += len;
+        }
+
+        // Set/return arcLengths
+        _arcLength = arcLength;
+        return _arcLengths = arcLengths;
+    }
+
+    @Override
+    protected void shapeChanged()
+    {
+        super.shapeChanged();
+
     }
 
     /**
