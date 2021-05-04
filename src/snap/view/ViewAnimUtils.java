50afcc2db61e5c39183729de60dc87c42b1593e7
Jeff Martin <jeff@reportmill.com>
2021-05-04 17:13:51 -0500

    ViewAnimUtils: Fixed setVisible(false) issues

--- src/snap/view/ViewAnimUtils.java
+++ src/snap/view/ViewAnimUtils.java
@@ -12,45 +12,86 @@ public class ViewAnimUtils {
      */
     public static ViewAnim setVisible(View aView, boolean aValue, boolean doWidth, boolean doHeight)
     {
+        // If already set, just return
+        if (aValue == aView.isVisible())
+            return aView.getAnim(0);
+
         // Get animator
         ViewAnim anim = aView.getAnim(0).finish().clear().getAnim(500);
         anim.setInterpolator(Interpolator.EASE_OUT);
 
         // Handle show inspector
         if (aValue) {
-            double prefW = aView.isPrefWidthSet() ? aView.getPrefWidth() : -1;
-            double prefH = aView.isPrefHeightSet() ? aView.getPrefHeight() : -1;
-            if (doWidth)
-                aView.setPrefWidth(1);
-            if (doHeight)
-                aView.setPrefHeight(1);
+
+            // Get old PrefW/PrefH values
+            double oldPrefW = aView.isPrefWidthSet() ? aView.getPrefWidth() : -1;
+            double oldPrefH = aView.isPrefHeightSet() ? aView.getPrefHeight() : -1;
+            boolean oldClipToBounds = aView.isClipToBounds();
+
+            // Make sure view is visible but transparent
             aView.setVisible(true);
-            if (doWidth)
+            aView.setOpacity(0);
+            aView.setClipToBounds(true);
+
+            // If animating width, set PrefWidth from 1 to actual PrefWidth
+            if (doWidth) {
+                double prefW = aView.getPrefWidth();
+                aView.setPrefWidth(1);
                 anim.setPrefWidth(prefW);
-            if (doHeight)
+            }
+
+            // If animating width, set PrefWidth from 1 to actual PrefWidth
+            if (doHeight) {
+                double prefH = aView.getPrefHeight();
+                aView.setPrefHeight(1);
                 anim.setPrefHeight(prefH);
+            }
+
+            // Animate opacity to 1
             anim.setOpacity(1);
+
+            // On Finish, restore old PrefW, PrefH, ClipToBounds
             anim.setOnFinish(() -> {
-                aView.setPrefWidth(prefW);
-                aView.setPrefHeight(prefH);
+                aView.setPrefWidth(oldPrefW);
+                aView.setPrefHeight(oldPrefH);
+                aView.setClipToBounds(oldClipToBounds);
             });
+
+            // Start anim
             anim.play();
         }
 
         // Handle hide inspector
         else {
+
+            // Get old PrefW/PrefH values
             double prefW = aView.isPrefWidthSet() ? aView.getPrefWidth() : -1;
             double prefH = aView.isPrefHeightSet() ? aView.getPrefHeight() : -1;
+            boolean oldClipToBounds = aView.isClipToBounds();
+
+            // Make sure view is clipping to bounds
+            aView.setClipToBounds(true);
+
+            // If animating width, set PrefWidth to 1
             if (doWidth)
                 anim.setPrefWidth(1);
+
+            // If animating height, set PrefHeight to 1
             if (doHeight)
                 anim.setPrefHeight(1);
+
+            // Animate opacity to zero
             anim.setOpacity(0);
+
+            // On finish, make not visible and restore old PrefW, PrefH, ClipToBounds
             anim.setOnFinish(a -> {
                 aView.setVisible(false);
                 aView.setPrefWidth(prefW);
                 aView.setPrefHeight(prefH);
+                aView.setClipToBounds(oldClipToBounds);
             });
+
+            // Start anim
             anim.play();
         }
 
