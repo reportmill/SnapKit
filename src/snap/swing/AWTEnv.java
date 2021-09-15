5079330a90850d2deaeb4005adff0a35a926cc40
Jeff Martin <jeff@reportmill.com>
2021-09-15 14:55:00 -0500

    AWTEnv: Some formatting and fixed debug message

--- src/snap/swing/AWTEnv.java
+++ src/snap/swing/AWTEnv.java
@@ -16,16 +16,19 @@ import snap.web.*;
  * A GFXEnv implementation using AWT.
  */
 public class AWTEnv extends GFXEnv {
-    
+
+    // The current screen scale (1 = normal, 2 = HiDPI/Retina)
+    private static double  _screenScale = -1;
+
     // The shared AWTEnv
-    static AWTEnv     _shared;
+    private static AWTEnv     _shared;
 
     /**
      * Creates a new AWTEnv.
      */
     public AWTEnv()
     {
-        if (_env==null) {
+        if (_env == null) {
             _env = _shared = this;
             ColorSpace._factory = new AWTColorSpaceFactory();
         }
@@ -54,34 +57,49 @@ public class AWTEnv extends GFXEnv {
     /**
      * Returns a list of all system fontnames (excludes any that don't start with capital A-Z).
      */
-    public String[] getFontNames()  { return AWTFontUtils.getFontNames(); }
+    public String[] getFontNames()
+    {
+        return AWTFontUtils.getFontNames();
+    }
 
     /**
      * Returns a list of all system family names.
      */
-    public String[] getFamilyNames()  { return AWTFontUtils.getFamilyNames(); }
+    public String[] getFamilyNames()
+    {
+        return AWTFontUtils.getFamilyNames();
+    }
 
     /**
      * Returns a list of all font names for a given family name.
      */
-    public String[] getFontNames(String aFamilyName)  { return AWTFontUtils.getFontNames(aFamilyName); }
+    public String[] getFontNames(String aFamilyName)
+    {
+        return AWTFontUtils.getFontNames(aFamilyName);
+    }
 
     /**
      * Returns a font file for given name.
      */
-    public FontFile getFontFile(String aName)  { return new AWTFontFile(aName); }
+    public FontFile getFontFile(String aName)
+    {
+        return new AWTFontFile(aName);
+    }
 
     /**
      * Creates image from source.
      */
-    public Image getImage(Object aSource)  { return new J2DImage(aSource); }
+    public Image getImage(Object aSource)
+    {
+        return new J2DImage(aSource);
+    }
 
     /**
      * Creates image for width, height and alpha and dpi scale (0 = screen dpi, 1 = 72 dpi, 2 = 144 dpi).
      */
     public Image getImageForSizeAndScale(double aWidth, double aHeight, boolean hasAlpha, double aScale)
     {
-        double scale = aScale<=0? getScreenScale() : aScale;
+        double scale = aScale <= 0? getScreenScale() : aScale;
         return new J2DImage(aWidth, aHeight, hasAlpha, scale);
     }
 
@@ -92,7 +110,7 @@ public class AWTEnv extends GFXEnv {
     {
         try {
             Class cls = Class.forName("snap.swing.JFXSoundClip");
-            return (SoundClip)cls.getDeclaredConstructor(Object.class).newInstance(aSource);
+            return (SoundClip) cls.getDeclaredConstructor(Object.class).newInstance(aSource);
         }
         catch(Exception e) { throw new RuntimeException(e); }
     }
@@ -104,7 +122,7 @@ public class AWTEnv extends GFXEnv {
     {
         try {
             Class cls = Class.forName("snap.swing.JFXSoundClip");
-            return (SoundClip)cls.newInstance();
+            return (SoundClip) cls.newInstance();
         }
         catch(Exception e) { throw new RuntimeException(e); }
     }
@@ -115,8 +133,10 @@ public class AWTEnv extends GFXEnv {
     public void openFile(Object aSource)
     {
         // Get file
-        if (aSource instanceof WebFile) aSource = ((WebFile)aSource).getJavaFile();
-        if (aSource instanceof WebURL) aSource = ((WebURL)aSource).getJavaURL();
+        if (aSource instanceof WebFile)
+            aSource = ((WebFile)aSource).getJavaFile();
+        if (aSource instanceof WebURL)
+            aSource = ((WebURL)aSource).getJavaURL();
         File file = FileUtils.getFile(aSource);
 
         // Open with Desktop API
@@ -131,7 +151,7 @@ public class AWTEnv extends GFXEnv {
     {
         // Get URL string
         WebURL url = WebURL.getURL(aSource);
-        String urls = url!=null? url.getString() : null;
+        String urls = url != null? url.getString() : null;
 
         // Open with Desktop API
         try { Desktop.getDesktop().browse(new URI(urls)); }
@@ -144,8 +164,10 @@ public class AWTEnv extends GFXEnv {
     public void openTextFile(Object aSource)
     {
         // Get file
-        if (aSource instanceof WebFile) aSource = ((WebFile)aSource).getJavaFile();
-        if (aSource instanceof WebURL) aSource = ((WebURL)aSource).getJavaURL();
+        if (aSource instanceof WebFile)
+            aSource = ((WebFile)aSource).getJavaFile();
+        if (aSource instanceof WebURL)
+            aSource = ((WebURL)aSource).getJavaURL();
         File file = FileUtils.getFile(aSource);
 
         // Open with Runtime.exec "open -e <file-name>"
@@ -168,6 +190,15 @@ public class AWTEnv extends GFXEnv {
      */
     public double getScreenScale()
     {
+        if (_screenScale >= 0) return _screenScale;
+        return _screenScale = getScreenScaleImpl();
+    }
+
+    /**
+     * Returns the screen scale. Usually 1, but could be 2 for HiDPI/Retina displays.
+     */
+    private double getScreenScaleImpl()
+    {
         // Get graphics configuration
         GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
         GraphicsDevice gd = ge.getDefaultScreenDevice();
@@ -175,28 +206,44 @@ public class AWTEnv extends GFXEnv {
         // The method was private in Java 8.
         try {
             Method  meth = gd.getClass().getMethod("getScaleFactor");
-            Number scale = (Number)meth.invoke(gd);
-            double ds = scale.doubleValue(); if (ds==1 || ds==2) return ds;
-            System.err.println("AWTEnv.getScreenScale: Unexepected value: " + ds); return 1;
+            Number scale = (Number) meth.invoke(gd);
+            double ds = scale.doubleValue();
+            if (ds == 1 || ds == 2)
+                return ds;
+            System.err.println("AWTEnv.getScreenScale: Unexpected value: " + ds);
+            return 1;
         }
-        catch(Exception e) { System.out.println("AWTEnv.getScreenScale: Unexepected error: " + e); }
+
+        // Handle exception
+        catch(Exception e) { }
 
         // This is the way to do it in Java 9.
         GraphicsConfiguration gc = gd.getDefaultConfiguration();
         AffineTransform xfm = gc.getDefaultTransform();
-        double ds = xfm.getScaleX(); if (ds==1 || ds==2) return ds;
-        System.err.println("AWTEnv.getScreenScale: Unexepected value: " + ds); return 1;
+        double ds = xfm.getScaleX();
+        if (ds == 1 || ds == 2)
+            return ds;
+
+        // Complain and return 1 since other methods failed
+        System.err.println("AWTEnv.getScreenScale: Unexpected value: " + ds);
+        return 1;
     }
 
     /**
      * Plays a beep.
      */
-    public void beep()  { Toolkit.getDefaultToolkit().beep(); }
+    public void beep()
+    {
+        Toolkit.getDefaultToolkit().beep();
+    }
 
     /**
      * Override to return AWTPrefs for name.
      */
-    public Prefs getPrefs(String aName)  { return AWTPrefs.getPrefs(aName); }
+    public Prefs getPrefs(String aName)
+    {
+        return AWTPrefs.getPrefs(aName);
+    }
 
     /**
      * This is really just here to help with TeaVM.
@@ -221,7 +268,7 @@ public class AWTEnv extends GFXEnv {
     {
         try {
             InetAddress h = InetAddress.getLocalHost();
-            return h==null ? "localhost" : h.getHostName();
+            return h == null ? "localhost" : h.getHostName();
         }
         catch(Exception e) { return "localhost"; }
     }
@@ -231,14 +278,17 @@ public class AWTEnv extends GFXEnv {
      */
     public static AWTEnv get()
     {
-        if (_shared!=null) return _shared;
+        if (_shared != null) return _shared;
         return new AWTEnv();
     }
 
     /**
      * Sets AWTEnv to be the default env.
      */
-    public static void set()  { get(); }
+    public static void set()
+    {
+        get();
+    }
 
     /**
      * Implementation of snap ColorSpace using java.awt.color.ColorSpace.
@@ -252,9 +302,11 @@ public class AWTEnv extends GFXEnv {
         public ColorSpace createColorSpaceICC(Object aSource)
         {
             // Get bytes
-            byte bytes[] = SnapUtils.getBytes(aSource);
-            if (bytes==null) {
-                System.err.println("AWTColorSpaceFactory: Error getting bytes for source: " + aSource); return null; }
+            byte[] bytes = SnapUtils.getBytes(aSource);
+            if (bytes == null) {
+                System.err.println("AWTColorSpaceFactory: Error getting bytes for source: " + aSource);
+                return null;
+            }
 
             // Load profile and create/return space
             try {
@@ -262,7 +314,10 @@ public class AWTEnv extends GFXEnv {
                 java.awt.color.ColorSpace acs = new java.awt.color.ICC_ColorSpace(prof);
                 return new AWTColorSpace(acs);
             }
-            catch(Exception e) { System.err.println("AWTColorSpaceFactory: Error reading colorspace: " + e); return null; }
+            catch(Exception e) {
+                System.err.println("AWTColorSpaceFactory: Error reading colorspace: " + e);
+                return null;
+            }
         }
     }
 
