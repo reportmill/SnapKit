c99a0d85df8bff7171b99ff76355f5c768326df1
Jeff Martin <jeff@reportmill.com>
2022-02-03 17:31:59 -0600

    Minor refactor

--- src/snap/gfx3d/Path3D.java
+++ src/snap/gfx3d/Path3D.java
@@ -479,7 +479,10 @@ public class Path3D extends Shape3D implements Cloneable {
     /**
      * Returns layers to be drawn in front of this path.
      */
-    public List <Path3D> getLayers()  { return _layers!=null ? _layers : Collections.EMPTY_LIST; }
+    public List <Path3D> getLayers()
+    {
+        return _layers != null ? _layers : Collections.EMPTY_LIST;
+    }
 
     /**
      * Adds a path to be drawn immediately in front of this path.
@@ -502,53 +505,9 @@ public class Path3D extends Shape3D implements Cloneable {
     }
 
     /**
-     * Compares ZMin for this path and given path.
+     * Returns the distance from a point to the plane of this path.
      */
-    public int compareZMin(Path3D path2)
-    {
-        double z0 = getMinZ();
-        double z1 = path2.getMinZ();
-        return z0 < z1 ? Sort3D.ORDER_BACK_TO_FRONT : z1 < z0 ? Sort3D.ORDER_FRONT_TO_BACK : 0;
-    }
-
-    /**
-     * Returns whether this path is in front (FRONT_TO_BACK) or aPath in front (BACK_TO_FRONT).
-     * Returns ORDER_SAME if the two paths are coplanar, or INDETERMINATE if they intersect.
-     */
-    public int comparePlane(Path3D aPath)
-    {
-        // Iterate over path points, get distance for each to plane, if distance ever flips sign
-        double pathDist = 0;
-        for (int i = 0, iMax = aPath.getPointCount(); i < iMax; i++) {
-
-            // Get distance from path point to plane
-            Point3D point = aPath.getPoint(i);
-            double pointDist = getDistance(point);
-            if (pathDist == 0)
-                pathDist = pointDist;
-
-            // If distance from loop point is opposite side of path plane (sign flipped), return indeterminate
-            boolean pointsOnBothSidesOfPlane = pointDist != 0 && pointDist * pathDist < 0;
-            if (pointsOnBothSidesOfPlane)
-                return Sort3D.ORDER_INEDETERMINATE;
-        }
-
-        // If given path is positive distance from this path, return
-        if (pathDist > 0)
-            return Sort3D.ORDER_FRONT_TO_BACK;
-
-        // If given path is positive distance from this path, return
-        if (pathDist < 0)
-            return Sort3D.ORDER_BACK_TO_FRONT;
-
-        // Planes are co-planar
-        return Sort3D.ORDER_SAME;
-    }
-
-    /**
-     * Returns the distance from a point to the plane of this polygon.
-     */
-    public double getDistance(Point3D aPoint)
+    public double getDistanceFromPointToPathPlane(Point3D aPoint)
     {
         // Get plane normal
         Vector3D planeNormal = getNormal();
@@ -560,7 +519,7 @@ public class Path3D extends Shape3D implements Cloneable {
         double vz = aPoint.z - planePoint.z;
 
         // Distance is just the length of the projection of points vector onto normal vector (v dot n)
-        double dist = vx * planeNormal.x + vy * planeNormal.y + vz + planeNormal.z;
+        double dist = vx * planeNormal.x + vy * planeNormal.y + vz * planeNormal.z;
         return Math.abs(dist) < .001 ? 0 : dist;
     }
 
--- src/snap/gfx3d/Sort3D.java
+++ src/snap/gfx3d/Sort3D.java
@@ -21,60 +21,93 @@ public class Sort3D {
      */
     public static void sortPaths(List<Path3D> thePaths)
     {
-        // Get list of paths and sort from front to back with simple Z min sort
-        Collections.sort(thePaths, (p0, p1) -> p0.compareZMin(p1));
+        Collections.sort(thePaths, (p0, p1) -> comparePath3Ds(p0, p1));
+        return;
+    }
 
-        // Sort again front to back with exhaustive sort satisfying Depth Sort Algorithm
-        for (int i = thePaths.size() - 1; i > 0; i--) {
+    /**
+     * Compares two paths.
+     */
+    private static int comparePath3Ds(Path3D path1, Path3D path2)
+    {
+        // If all path1 points are behind all path2 points, return BACK_TO_FRONT
+        if (path1.getMaxZ() <= path2.getMinZ())
+            return ORDER_BACK_TO_FRONT;
 
-            // Get loop path
-            Path3D path1 = thePaths.get(i);
-            int i2 = i;
+        // If all path1 points are in front of all path2 points, return FRONT_TO_BACK
+        if (path1.getMinZ() >= path2.getMaxZ())
+            return ORDER_FRONT_TO_BACK;
 
-            // Iterate over remaining paths
-            for (int j = 0; j < i; j++) {
-
-                // Get loop path (if same path, just skip)
-                Path3D path2 = thePaths.get(j);
-                if (path2 == path1)
-                    continue;
-
-                // If no X/Y/Z overlap, just continue
-                if (path1.getMinZ() >= path2.getMaxZ())
-                    continue;
-                if (path1.getMaxX() <= path2.getMinX() || path1.getMinX() >= path2.getMaxX())
-                    continue;
-                if (path1.getMaxY() <= path2.getMinY() || path1.getMinY() >= path2.getMaxY())
-                    continue;
-
-                // Test path planes - if on same plane or in correct order, they don't overlap
-                int comp1 = path1.comparePlane(path2);
-                if (comp1 == ORDER_SAME || comp1 == ORDER_BACK_TO_FRONT)
-                    continue;
-                int comp2 = path2.comparePlane(path1);
-                if (comp2 == ORDER_FRONT_TO_BACK)
-                    continue;
-
-                // If 2d paths don't intersect, just continue
-                if (!path1.getPath().intersects(path2.getPath(),0))
-                    continue;
-
-                // If all five tests fail, try next path up from path1
-                if (i2 == 0) {  // Not sure why this can happen
-                    System.err.println("Path3D.sort: Sort fail.");
-                    i = 0;
-                }
-                else {
-                    path1 = thePaths.get(--i2);
-                    j = -1;
-                }
-            }
-
-            // Move poly
-            if (i2 != i) {
-                thePaths.remove(i2);
-                thePaths.add(i, path1);
-            }
+        // If no X/Y overlap, return MinZ order
+        if (path1.getMaxX() <= path2.getMinX() || path1.getMinX() >= path2.getMaxX() ||
+            path1.getMaxY() <= path2.getMinY() || path1.getMinY() >= path2.getMaxY()) {
+            int compZ = comparePath3D_MinZs(path1, path2);
+            return compZ;
         }
+
+        // If all path2 points in front or back of path1, return that order
+        int comp1 = comparePath3D_Planes(path1, path2);
+        if (comp1 == ORDER_BACK_TO_FRONT || comp1 == ORDER_FRONT_TO_BACK)
+            return comp1;
+
+        // If path1/path2 points are coplanar, return MinZ order
+        if (comp1 == ORDER_SAME)
+            return comparePath3D_MinZs(path1, path2);
+
+        // If all path1 points in front or back of path2, return reverse order
+        int comp2 = comparePath3D_Planes(path2, path1);
+        if (comp2 == ORDER_BACK_TO_FRONT)
+            return ORDER_FRONT_TO_BACK;
+        if (comp2 == ORDER_FRONT_TO_BACK)
+            return ORDER_BACK_TO_FRONT;
+
+        // This should never happen
+        System.err.println("Path3D.sort: Sort fail.");
+        int compZ = comparePath3D_MinZs(path1, path2);
+        return compZ;
+    }
+
+    /**
+     * Compares given paths MinZ values.
+     */
+    private static int comparePath3D_MinZs(Path3D path1, Path3D path2)
+    {
+        double z0 = path1.getMinZ();
+        double z1 = path2.getMinZ();
+        return z0 < z1 ? Sort3D.ORDER_BACK_TO_FRONT : z1 < z0 ? Sort3D.ORDER_FRONT_TO_BACK : 0;
+    }
+
+    /**
+     * Returns whether path1/path are ordered BACK_TO_FRONT OR FRONT_TO_BACK.
+     * Returns ORDER_SAME if the two paths are coplanar, or INDETERMINATE if they intersect.
+     */
+    private static int comparePath3D_Planes(Path3D path1, Path3D path2)
+    {
+        // Iterate over path points, get distance for each to plane, if distance ever flips sign
+        double path2Dist = 0;
+        for (int i = 0, iMax = path2.getPointCount(); i < iMax; i++) {
+
+            // Get distance from path point to plane
+            Point3D point = path2.getPoint(i);
+            double pointDist = path1.getDistanceFromPointToPathPlane(point);
+            if (path2Dist == 0)
+                path2Dist = pointDist;
+
+            // If distance from loop point is opposite side of path plane (sign flipped), return indeterminate
+            boolean pointsOnBothSidesOfPlane = pointDist != 0 && pointDist * path2Dist < 0;
+            if (pointsOnBothSidesOfPlane)
+                return Sort3D.ORDER_INEDETERMINATE;
+        }
+
+        // If path1 to path2 distance is positive, return BACK_TO_FRONT
+        if (path2Dist > 0)
+            return Sort3D.ORDER_BACK_TO_FRONT;
+
+        // If path1 to path2 distance is negative, return FRONT_TO_BACK
+        if (path2Dist < 0)
+            return Sort3D.ORDER_FRONT_TO_BACK;
+
+        // Planes are co-planar
+        return Sort3D.ORDER_SAME;
     }
 }
