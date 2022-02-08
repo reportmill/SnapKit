850cd5b19c4621d30c8a18abd1cc7fb4d4abd4b1
Jeff Martin <jeff@reportmill.com>
2022-02-08 15:37:21 -0600

    Minor refactor

--- src/snap/gfx3d/Sort3D.java
+++ src/snap/gfx3d/Sort3D.java
@@ -38,26 +38,32 @@ public class Sort3D {
         if (path1.getMinZ() >= path2.getMaxZ())
             return ORDER_FRONT_TO_BACK;
 
+        // Get simple Z min order
+        int compZ = comparePath3D_MinZs(path1, path2);
+
         // If no X/Y overlap, return MinZ order
         if (path1.getMaxX() <= path2.getMinX() || path1.getMinX() >= path2.getMaxX() ||
             path1.getMaxY() <= path2.getMinY() || path1.getMinY() >= path2.getMaxY()) {
-            int compZ = comparePath3D_MinZs(path1, path2);
             return compZ;
         }
 
-        // If all path2 points in front or back of path1, return that order
+        // Get ordering based on whether path (2) points are in front or behind path (1) plane
         int comp1 = comparePath3D_Planes(path1, path2);
+        int comp2 = comparePath3D_Planes(path2, path1);
+
+        // If both paths find each other in front or back, just return Z min order (they probably share a side)
+        if (comp1 == comp2 && comp1 != ORDER_INEDETERMINATE)
+            return compZ;
+
+        // If all path2 points in front or back of path1, return that order
         if (comp1 == ORDER_BACK_TO_FRONT || comp1 == ORDER_FRONT_TO_BACK)
             return comp1;
 
         // If path1/path2 points are coplanar, return MinZ order
-        if (comp1 == ORDER_SAME) {
-            int compZ = comparePath3D_MinZs(path1, path2);
+        if (comp1 == ORDER_SAME)
             return compZ;
-        }
 
         // If all path1 points in front or back of path2, return reverse order
-        int comp2 = comparePath3D_Planes(path2, path1);
         if (comp2 == ORDER_BACK_TO_FRONT)
             return ORDER_FRONT_TO_BACK;
         if (comp2 == ORDER_FRONT_TO_BACK)
@@ -65,7 +71,6 @@ public class Sort3D {
 
         // This should never happen
         System.err.println("Path3D.comparePath3Ds: Sort fail.");
-        int compZ = comparePath3D_MinZs(path1, path2);
         return compZ;
     }
 
