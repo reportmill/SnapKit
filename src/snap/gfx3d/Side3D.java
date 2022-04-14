ee6bb3a4e42483092f270ec00c1c87120da5e5f9
Jeff Martin <jeff@reportmill.com>
2022-04-14 12:57:04 -0500

    AxisBoxShape: Moved grid generation to separate methods: addSideGrids(), addSideGridFrontBack(), ... DataArea3D: Moved getAxisBoxPrefWidth/Height/Depth here from AxisBoxShape DataArea3D: Added getPrefIntervalsX/Y/Z to provide constant axis min/max values Side3D: Added getNormalInward() Intervals: Added equals() method

--- src/snap/gfx3d/Side3D.java
+++ src/snap/gfx3d/Side3D.java
@@ -29,4 +29,28 @@ public enum Side3D {
      * Returns whether side is TOP or BOTTOM.
      */
     public boolean isTopOrBottom()  { return this == TOP || this == BOTTOM; }
+
+    // Constants for side normals
+    private static final Vector3D FRONT_NORMAL = new Vector3D(0, 0, -1);
+    private static final Vector3D BACK_NORMAL = new Vector3D(0, 0, 1);
+    private static final Vector3D LEFT_NORMAL = new Vector3D(1, 0, 0);
+    private static final Vector3D RIGHT_NORMAL = new Vector3D(-1, 0, 0);
+    private static final Vector3D TOP_NORMAL = new Vector3D(0, -1, 0);
+    private static final Vector3D BOTTOM_NORMAL = new Vector3D(0, 1, 0);
+
+    /**
+     * Returns the side normal for a given side.
+     */
+    public Vector3D getNormalInward()
+    {
+        switch (this) {
+            case FRONT: return FRONT_NORMAL;
+            case BACK: return BACK_NORMAL;
+            case LEFT: return LEFT_NORMAL;
+            case RIGHT: return RIGHT_NORMAL;
+            case TOP: return TOP_NORMAL;
+            case BOTTOM: return BOTTOM_NORMAL;
+            default: throw new RuntimeException("Side3D.getNormalInward: Unknown side: " + this);
+        }
+    }
 }
