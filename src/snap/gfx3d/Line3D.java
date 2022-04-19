69c30ad12a3edb45281fe2c5bc4b0dfaf419c117
Jeff Martin <jeff@reportmill.com>
2022-04-18 20:51:50 -0500

    Line3D: Added getLength(), getLengthXY(), extendEndPointForFraction(), getAngle2DInDeg(), clone() Point3D: Added getDistance(), getDistanceSquared()

--- src/snap/gfx3d/Line3D.java
+++ src/snap/gfx3d/Line3D.java
@@ -7,7 +7,7 @@ import snap.geom.Point;
 /**
  * This class represents a 3D line.
  */
-public class Line3D {
+public class Line3D implements Cloneable{
 
     // The first point
     private Point3D  _p1;
@@ -45,8 +45,69 @@ public class Line3D {
     /**
      * Returns the distance between the XY components (assumes the line is in View space).
      */
-    public double getDistance2D()
+    public double getLength()
+    {
+        return Point3D.getDistance(_p1.x, _p1.y, _p1.z, _p2.x, _p2.y, _p2.z);
+    }
+
+    /**
+     * Returns the distance between the XY components (assumes the line is in View space).
+     */
+    public double getLengthXY()
     {
         return Point.getDistance(_p1.x, _p1.y, _p2.x, _p2.y);
+    }
+
+    /**
+     * Changes line end point for fractional value. E.g.: 2 doubles length of line.
+     */
+    public void extendEndPointForFraction(double aValue)
+    {
+        double length = getLength();
+        double length2 = length * aValue;
+        Vector3D dir = new Vector3D(_p1, _p2); dir.normalize();
+
+        if (aValue >= 0) {
+            _p2.x = _p1.x + dir.x * length2;
+            _p2.y = _p1.y + dir.y * length2;
+            _p2.z = _p1.z + dir.z * length2;
+        }
+        else {
+            _p1.x = _p2.x + dir.x * length2;
+            _p1.y = _p2.y + dir.y * length2;
+            _p1.z = _p2.z + dir.z * length2;
+        }
+    }
+
+    /**
+     * Returns the angle between two XY points.
+     */
+    public double getAngle2DInDeg()
+    {
+        return getAngleBetweenPoints2DInDeg(_p1.x, _p1.y, _p2.x, _p2.y);
+    }
+
+    /**
+     * Standard clone implementation.
+     */
+    public Line3D clone()
+    {
+        Line3D clone;
+        try { clone = (Line3D) super.clone(); }
+        catch (CloneNotSupportedException e) { throw new RuntimeException(e); }
+
+        clone._p1 = _p1.clone();
+        clone._p2 = _p2.clone();
+        return clone;
+    }
+
+    /**
+     * Returns the angle between two XY points.
+     */
+    public static double getAngleBetweenPoints2DInDeg(double x1, double y1, double x2, double y2)
+    {
+        double angle = Math.atan2(y2 - y1, x2 - x1);
+        double angleDeg = Math.toDegrees(angle);
+        return angleDeg;
     }
 }
--- src/snap/gfx3d/Point3D.java
+++ src/snap/gfx3d/Point3D.java
@@ -46,6 +46,22 @@ public class Point3D implements Cloneable {
     }
 
     /**
+     * Returns the distance to another point.
+     */
+    public double getDistance(Point3D aPoint)
+    {
+        return getDistance(aPoint.x, aPoint.y, aPoint.z);
+    }
+
+    /**
+     * Returns the distance to another point.
+     */
+    public double getDistance(double aX, double aY, double aZ)
+    {
+        return getDistance(x, y, z, aX, aY, aZ);
+    }
+
+    /**
      * Standard equals implementation.
      */
     public boolean equals(Object anObj)
@@ -68,5 +84,25 @@ public class Point3D implements Cloneable {
     public String toString()
     {
         return "Point3D [" + StringUtils.toString(x) + " " + StringUtils.toString(y) + " " + StringUtils.toString(z) + "]";
+    }
+
+    /**
+     * Returns the distance from this Point to a specified point.
+     */
+    public static final double getDistance(double x1, double y1, double z1, double x2, double y2, double z2)
+    {
+        double distSqr = getDistanceSquared(x1, y1, z1, x2, y2, z2);
+        return Math.sqrt(distSqr);
+    }
+
+    /**
+     * Returns the distance from this Point to a specified point.
+     */
+    public static final double getDistanceSquared(double x1, double y1, double z1, double x2, double y2, double z2)
+    {
+        double dx = x2 - x1;
+        double dy = y2 - y1;
+        double dz = z2 - z1;
+        return dx * dx + dy * dy + dz * dz;
     }
 }
