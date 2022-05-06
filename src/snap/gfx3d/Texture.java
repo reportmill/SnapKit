ce7843bbff643ee0a3e51e7178a11e698e7c50ad
Jeff Martin <jeff@reportmill.com>
2022-05-06 18:26:32 -0500

    Slight fix

--- src/snap/gfx3d/Texture.java
+++ src/snap/gfx3d/Texture.java
@@ -9,6 +9,9 @@ public class Texture {
     // The image
     private Image  _image;
 
+    // Whether the image is flipped
+    private boolean  _flipped;
+
     /**
      * Constructor.
      */
@@ -21,4 +24,14 @@ public class Texture {
      * Returns the image.
      */
     public Image getImage()  { return _image; }
+
+    /**
+     * Returns whether the image is flipped.
+     */
+    public boolean isFlipped()  { return _flipped; }
+
+    /**
+     * Sets whether the image is flipped.
+     */
+    public void setFlipped(boolean aValue)  { _flipped = aValue; }
 }
