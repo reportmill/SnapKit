aaf3db3761cbcd4c9a1e8b4f788a51f08ff8370a
Jeff Martin <jeff@reportmill.com>
2022-05-13 10:22:56 -0500

    PathBox3D: Fixed to flatten curveTo segments

--- src/snap/gfx3d/PathBox3D.java
+++ src/snap/gfx3d/PathBox3D.java
@@ -3,10 +3,7 @@
  */
 package snap.gfx3d;
 import java.util.*;
-
-import snap.geom.PathIter;
-import snap.geom.Point;
-import snap.geom.Shape;
+import snap.geom.*;
 import snap.gfx.*;
 
 /**
@@ -51,6 +48,7 @@ public class PathBox3D extends ParentShape {
         for (Shape3D shape3D : extrusionShapes) {
             shape3D.setColor(color);
             shape3D.setOpacity(opacity);
+            if (shape3D.getName() == null) continue;
             shape3D.setStrokeColor(strokeColor);
             shape3D.setStroke(stroke);
         }
@@ -64,17 +62,20 @@ public class PathBox3D extends ParentShape {
      */
     public static Shape3D[] createExtrusionShape3Ds(Shape aPath, double z1, double z2)
     {
+        // Get flattened path
+        Shape flatPath = aPath.getFlat();
+
         // Create list to hold paths
         List<Shape3D> paths = new ArrayList<>();
         Path3D back = null;
 
         // If path is closed, create path3d for front from aPath and z1
-        if (aPath.isClosed()) {
+        if (flatPath.isClosed()) {
 
             // Create path3d for front and back
-            Path3D front = new Path3D(aPath, z1);
+            Path3D front = new Path3D(flatPath, z1);
             front.setName("BoxBack");
-            back = new Path3D(aPath, z2);
+            back = new Path3D(flatPath, z2);
             back.setName("BoxFront");
 
             // Add front to paths list
@@ -87,7 +88,7 @@ public class PathBox3D extends ParentShape {
             // Otherwise, reverse back
             else {
                 back.reverse();
-                aPath = back.getShape2D();
+                flatPath = back.getShape2D();
             }
         }
 
@@ -99,61 +100,40 @@ public class PathBox3D extends ParentShape {
         int sideNum = 0;
 
         // Iterate over path elements
-        while (piter.hasNext()) switch (piter.getNext(pts)) {
+        while (piter.hasNext()) {
+            Seg seg = piter.getNext(pts);
+            switch (seg) {
 
-            // MoveTo
-            case MoveTo:
-                lastX = lastMoveX = pts[0];
-                lastY = lastMoveY = pts[1];
+                // Handle MoveTo:
+                case MoveTo:
+                    lastX = lastMoveX = pts[0];
+                    lastY = lastMoveY = pts[1];
+                    break;
+
+                // Handle LineTo:
+                case LineTo: {
+                    Poly3D polyShape = lineTo(lastX, lastY, lastX = pts[0], lastY = pts[1], z1, z2);
+                    polyShape.setName("BoxSide" + sideNum++);
+                    paths.add(polyShape);
+                }
                 break;
 
-            // LineTo
-            case LineTo: {
-                if (Point.equals(lastX, lastY, pts[0], pts[1])) continue;
-                Poly3D path = new Poly3D();
-                path.setName("BoxSide" + sideNum++);
-                path.addPoint(lastX, lastY, z1);
-                path.addPoint(pts[0], pts[1], z1);
-                path.addPoint(pts[0], pts[1], z2);
-                path.addPoint(lastX, lastY, z2);
-                paths.add(path);
-                lastX = pts[0]; lastY = pts[1];
-            } break;
+                // Handle CurveTo
+                case CubicTo:
+                    curveToFlat(lastX, lastY, pts[0], pts[1], pts[2], pts[3], lastX = pts[4], lastY = pts[5], z1, z2, paths);
+                    break;
 
-            // QuadTo
-            case QuadTo: {
-                Path3D path = new Path3D(); path.setName("BoxSide" + sideNum++);
-                path.moveTo(lastX, lastY, z1);
-                path.quadTo(pts[0], pts[1], z1, pts[2], pts[3], z1);
-                path.lineTo(pts[4], pts[5], z2);
-                path.quadTo(pts[0], pts[1], z2, lastX, lastY, z2);
-                path.close();
-                paths.add(path);
-                lastX = pts[2]; lastY = pts[3];
-            } break;
+                // Handle Close
+                case Close: {
+                    Poly3D polyShape = lineTo(lastX, lastY, lastX = lastMoveX, lastY = lastMoveY, z1, z2);
+                    polyShape.setName("BoxSide" + sideNum++);
+                    paths.add(polyShape);
+                }
+                break;
 
-            // CubicTo
-            case CubicTo: {
-                Path3D path = new Path3D(); path.setName("BoxSide" + sideNum++);
-                path.moveTo(lastX, lastY, z1);
-                path.curveTo(pts[0], pts[1], z1, pts[2], pts[3], z1, pts[4], pts[5], z1);
-                path.lineTo(pts[4], pts[5], z2);
-                path.curveTo(pts[2], pts[3], z2, pts[0], pts[1], z2, lastX, lastY, z2);
-                path.close();
-                paths.add(path);
-                lastX = pts[4]; lastY = pts[5];
-            } break;
-
-            // Close
-            case Close: {
-                Poly3D path = new Poly3D();
-                path.setName("BoxSide" + sideNum++);
-                path.addPoint(lastX, lastY, z1);
-                path.addPoint(lastMoveX, lastMoveY, z1);
-                path.addPoint(lastMoveX, lastMoveY, z2);
-                path.addPoint(lastX, lastY, z2);
-                paths.add(path);
-            } break;
+                // Handle unexpected
+                default: System.err.println("PathBox3D.createExtrusionShape3Ds: Unexpected path seg: " + seg);
+            }
         }
 
         // Add back face to paths
@@ -162,5 +142,41 @@ public class PathBox3D extends ParentShape {
 
         // Return paths
         return paths.toArray(new Shape3D[0]);
+    }
+
+    /**
+     * Create a Poly3D quad surface for 2D lineTo points and depth1/depth2.
+     */
+    private static Poly3D lineTo(double x1, double y1, double x2, double y2, double z1, double z2)
+    {
+        if (Point.equals(x1, y1, x2, y2)) return null;
+        Poly3D path = new Poly3D();
+        path.addPoint(x1, y1, z1);
+        path.addPoint(x2, y2, z1);
+        path.addPoint(x2, y2, z2);
+        path.addPoint(x1, y1, z2);
+        return path;
+    }
+
+    /**
+     * CubicTo by adding lineTo segments.
+     */
+    private static void curveToFlat(double lastX, double lastY, double cp1x, double cp1y, double cp2x, double cp2y, double x, double y,
+                          double z1, double z2, List<Shape3D> polyList)
+    {
+        // If distance from control points to base line less than tolerance, just add line
+        double dist1 = Line.getDistance(lastX, lastY, x, y, cp1x, cp1y);
+        double dist2 = Line.getDistance(lastX, lastY, x, y, cp2x, cp2y);
+        if (dist1 < .25 && dist2 < .25) {
+            Poly3D polyShape = lineTo(lastX, lastY, x, y, z1, z2);
+            polyList.add(polyShape);
+            return;
+        }
+
+        // Split curve at midpoint and add parts
+        Cubic c0 = new Cubic(lastX, lastY, cp1x, cp1y, cp2x, cp2y, x, y);
+        Cubic c1 = c0.split(.5);
+        curveToFlat(lastX, lastY, c0.cp0x, c0.cp0y, c0.cp1x, c0.cp1y, c0.x1, c0.y1, z1, z2, polyList);
+        curveToFlat(c0.x1, c0.y1, c1.cp0x, c1.cp0y, c1.cp1x, c1.cp1y, c1.x1, c1.y1, z1, z2, polyList);
     }
 }
