c4e2f0329767c0a88dd9e127eedbf0c012543868
Jeff Martin <jeff@reportmill.com>
2022-12-01 13:25:39 -0600

    Fixed warnings

--- src/snap/util/ArrayUtils.java
+++ src/snap/util/ArrayUtils.java
@@ -5,6 +5,7 @@ package snap.util;
 import java.lang.reflect.Array;
 import java.util.*;
 import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 /**
  * A collection of array utility methods.
@@ -14,141 +15,153 @@ public class ArrayUtils {
     /**
      * Returns the length of a given object array (or zero, if null).
      */
-    public static int length(Object anArray[])  { return anArray!=null ? anArray.length : 0; }
+    public static int length(Object[] anArray)
+    {
+        return anArray != null ? anArray.length : 0;
+    }
 
     /**
      * Adds a value to an array of objects.
      */
-    public static <T> T[] add(T anArray[], T anObj)  { return add(anArray, anObj, length(anArray)); }
+    public static <T> T[] add(T[] anArray, T anObj)
+    {
+        return add(anArray, anObj, length(anArray));
+    }
 
     /**
      * Returns a new array by adding a value to an array of objects at the given index.
      */
-    public static <T> T[] add(T anArray[], T anObj, int anIndex)
+    public static <T> T[] add(T[] anArray, T anObj, int anIndex)
     {
         // Get array length (throw exception if index out of bounds)
         int length = anArray.length;
-        if (anIndex<0 || anIndex>length)
+        if (anIndex < 0 || anIndex > length)
             throw new ArrayIndexOutOfBoundsException(anIndex);
 
         // Create new array,
-        T newArray[] = (T[])Array.newInstance(anArray.getClass().getComponentType(), length+1);
+        T[] newArray = Arrays.copyOf(anArray, length + 1);
 
         // Copy elements prior to index, add value at index, and copy elements beyond index
-        System.arraycopy(anArray, 0, newArray, 0, anIndex);
         newArray[anIndex] = anObj;
-        if (length>anIndex)
-            System.arraycopy(anArray, anIndex, newArray, anIndex+1, length - anIndex);
+        if (length > anIndex)
+            System.arraycopy(anArray, anIndex, newArray, anIndex + 1, length - anIndex);
 
-        // Return new array
+        // Return
         return newArray;
     }
 
     /**
      * Returns an array by adding to the given array the given objects (array).
      */
-    public static <T> T[] addAll(T anArray[], T ... theObjs)
+    public static <T> T[] addAll(T[] anArray, T ... theObjs)
     {
-        if (anArray==null) return theObjs;
-        T array[] = (T[])Array.newInstance(anArray.getClass().getComponentType(), anArray.length + theObjs.length);
-        System.arraycopy(anArray, 0, array, 0, anArray.length);
-        System.arraycopy(theObjs, 0, array, anArray.length, theObjs.length);
-        return array;
-    }
+        if (anArray == null) return theObjs;
+        if (theObjs.length == 0) return anArray;
 
-    /**
-     * Returns a new array by removing a value from an array of objects at the given index.
-     */
-    public static <T> T[] remove(T anArray[], int anIndex)
-    {
-        // Get array length (just return new single object array if empty)
-        int length = anArray.length;
-        if (anIndex<0 || anIndex>=length)
-            throw new ArrayIndexOutOfBoundsException(anIndex);
-
-        // Create new array
-        T newArray[] = (T[])Array.newInstance(anArray.getClass().getComponentType(), length-1);
-
-        // Copy elements prior to index and copy elements beyond index
-        System.arraycopy(anArray, 0, newArray, 0, anIndex);
-        if (length>anIndex+1)
-            System.arraycopy(anArray, anIndex+1, newArray, anIndex, length - anIndex - 1);
-
-        // Return new array
+        T[] newArray = Arrays.copyOf(anArray, anArray.length + theObjs.length);
+        System.arraycopy(theObjs, 0, newArray, anArray.length, theObjs.length);
         return newArray;
     }
 
     /**
      * Returns a new array by removing a value from an array of objects at the given index.
      */
-    public static <T> T[] remove(T anArray[], T anObj)
+    public static <T> T[] remove(T[] anArray, int anIndex)
     {
-        int index = indexOf(anArray, anObj);
-        return index>=0 ? remove(anArray, index) : anArray;
+        // Get array length (just return new single object array if empty)
+        int length = anArray.length;
+        if (anIndex < 0 || anIndex >= length)
+            throw new ArrayIndexOutOfBoundsException(anIndex);
+
+        // Create new array
+        T[] newArray = (T[]) Array.newInstance(anArray.getClass().getComponentType(), length - 1);
+
+        // Copy elements prior to index and copy elements beyond index
+        System.arraycopy(anArray, 0, newArray, 0, anIndex);
+        if (length > anIndex + 1)
+            System.arraycopy(anArray, anIndex + 1, newArray, anIndex, length - anIndex - 1);
+
+        // Return
+        return newArray;
     }
 
     /**
      * Returns a new array by removing a value from an array of objects at the given index.
      */
-    public static int[] remove(int anArray[], int anIndex)
+    public static <T> T[] remove(T[] anArray, T anObj)
+    {
+        int index = indexOf(anArray, anObj);
+        return index >= 0 ? remove(anArray, index) : anArray;
+    }
+
+    /**
+     * Returns a new array by removing a value from an array of objects at the given index.
+     */
+    public static int[] remove(int[] anArray, int anIndex)
     {
         // Get array length (just return new single object array if empty)
         int length = anArray.length;
-        if (anIndex<0 || anIndex>=length)
+        if (anIndex < 0 || anIndex >= length)
             throw new ArrayIndexOutOfBoundsException(anIndex);
 
         // Create new array and copy elements beyond index
-        int newArray[] = Arrays.copyOf(anArray, length-1);
-        if (length>anIndex+1)
-            System.arraycopy(anArray, anIndex+1, newArray, anIndex, length - anIndex - 1);
+        int[] newArray = Arrays.copyOf(anArray, length - 1);
+        if (length > anIndex + 1)
+            System.arraycopy(anArray, anIndex + 1, newArray, anIndex, length - anIndex - 1);
 
-        // Return new array
+        // Return
         return newArray;
     }
 
     /**
      * Replaces a range of values in an int array with values from another int array.
      */
-    public static <T> T[] replace(T anArray[], int start, int end, T ... swapInArray)
+    public static <T> T[] replace(T[] anArray, int start, int end, T ... swapInArray)
     {
         // Get lengths
         int swapOutLength = end - start;
-        int swapInLength = swapInArray==null ? 0 : swapInArray.length;
+        int swapInLength = swapInArray.length;
         int newLength = anArray.length - swapOutLength + swapInLength;
 
         // Create new array
-        T newArray[] = (T[])Array.newInstance(anArray.getClass().getComponentType(), newLength);
+        T[] newArray = Arrays.copyOf(anArray, newLength);
 
-        // Copy elements prior to index
-        System.arraycopy(anArray, 0, newArray, 0, start);
+        // Copy elements from swap array and from tail of original array
         System.arraycopy(swapInArray, 0, newArray, start, swapInLength);
         System.arraycopy(anArray, end, newArray, start + swapInLength, anArray.length - end);
 
-        // Return new array
+        // Return
         return newArray;
     }
 
     /**
      * Adds a value to an array of objects if list doesn't already contain it.
      */
-    public static <T> T[] addId(T anArray[], T anObj)
+    public static <T> T[] addId(T[] anArray, T anObj)
     {
-        return containsId(anArray, anObj) ? anArray : add(anArray, anObj);
+        if (containsId(anArray, anObj))
+            return anArray;
+        return add(anArray, anObj);
     }
 
     /**
      * Removes a value from an array of objects if list doesn't already contain it.
      */
-    public static <T> T[] removeId(T anArray[], T anObj)
+    public static <T> T[] removeId(T[] anArray, T anObj)
     {
         int index = indexOfId(anArray, anObj);
-        return index>=0 ? remove(anArray, index) : anArray;
+        if (index >= 0)
+            return remove(anArray, index);
+        return anArray;
     }
 
     /**
      * Returns new double array by adding given value to given array.
      */
-    public static double[] add(double[] anArray, double anObj)  { return add(anArray, anObj, anArray.length); }
+    public static double[] add(double[] anArray, double anObj)
+    {
+        return add(anArray, anObj, anArray.length);
+    }
 
     /**
      * Returns new double array by adding given value to given array at given index.
@@ -175,13 +188,13 @@ public class ArrayUtils {
     /**
      * Returns the number of object in array of a given class.
      */
-    public static int getCount(Object anArray[], Class aClass)
+    public static int getCount(Object[] anArray, Class<?> aClass)
     {
         // Declare local variable for count
         int count = 0;
 
         // Iterate over all listeners and increment for count
-        for (int i=0, iMax=length(anArray); i<iMax; i++)
+        for (int i = 0, iMax = length(anArray); i < iMax; i++)
             if (aClass.isAssignableFrom(anArray[i].getClass()))
                 count++;
 
@@ -192,13 +205,13 @@ public class ArrayUtils {
     /**
      * Returns the individual object of a given class at given index (from all objects of given class).
      */
-    public static <T> T get(Object anArray[], Class<T> aClass, int anIndex)
+    public static <T> T get(Object[] anArray, Class<T> aClass, int anIndex)
     {
         // Iterate over all until index of listener of given class is found
-        for (int i=0, iMax=anArray.length, j=0; i<iMax; i++)
+        for (int i = 0, iMax = anArray.length, j = 0; i < iMax; i++)
             if (aClass.isAssignableFrom(anArray[i].getClass()))
-                if (j++==anIndex)
-                    return (T)anArray[i];
+                if (j++ == anIndex)
+                    return (T) anArray[i];
 
         // throw array out of bounds
         throw new ArrayIndexOutOfBoundsException(anIndex);
@@ -207,29 +220,19 @@ public class ArrayUtils {
     /**
      * Returns the individual object of a given class at given index (from all objects of given class).
      */
-    public static <T> T[] get(Object anArray[], Class<T> aClass)
+    public static <T> T[] get(Object[] anArray, Class<T> aClass)
     {
         // Get size and create array
         int size = getCount(anArray, aClass);
-        T array[] = (T[])Array.newInstance(aClass, size);
+        T[] newArray = (T[]) Array.newInstance(aClass, size);
 
         // Iterate over all until index of listener of given class is found
-        for (int i=0, iMax=anArray.length, j=0; i<iMax && j<size; i++)
+        for (int i = 0, iMax = anArray.length, j = 0; i < iMax && j < size; i++)
             if (aClass.isAssignableFrom(anArray[i].getClass()))
-                array[j++] = (T)anArray[i];
+                newArray[j++] = (T) anArray[i];
 
-        // Return array
-        return array;
-    }
-
-    /**
-     * Returns an array as an ArrayList.
-     */
-    public static <T> ArrayList <T> asArrayList(T ... theItems)
-    {
-        ArrayList <T> list = new ArrayList();
-        for (T item : theItems) list.add(item);
-        return list;
+        // Return
+        return newArray;
     }
 
     /**
@@ -251,7 +254,7 @@ public class ArrayUtils {
         if (array1 == array2) return true;
         if (array1 == null || array2 == null || array1.length < length || array2.length < length)
             return false;
-        for (int i=0; i<length; i++)
+        for (int i = 0; i < length; i++)
             if (array1[i] != array2[i])
                 return false;
         return true;
@@ -276,7 +279,7 @@ public class ArrayUtils {
         if (array1 == array2) return true;
         if (array1 == null || array2 == null || array1.length < length || array2.length < length)
             return false;
-        for (int i=0; i<length; i++)
+        for (int i = 0; i < length; i++)
             if (array1[i] != array2[i])
                 return false;
         return true;
@@ -301,7 +304,7 @@ public class ArrayUtils {
         if (array1 == array2) return true;
         if (array1 == null || array2 == null || array1.length < length || array2.length < length)
             return false;
-        for (int i=0; i<length; i++)
+        for (int i = 0; i < length; i++)
             if (array1[i] != array2[i])
                 return false;
         return true;
@@ -314,7 +317,7 @@ public class ArrayUtils {
     {
         if (theFloats == null) return null;
         double[] doubleArray = new double[theFloats.length];
-        for (int i=0; i<theFloats.length; i++) doubleArray[i] = theFloats[i];
+        for (int i = 0; i < theFloats.length; i++) doubleArray[i] = theFloats[i];
         return doubleArray;
     }
 
@@ -323,9 +326,9 @@ public class ArrayUtils {
      */
     public static float[] floatArray(double[] theDoubles)
     {
-        if (theDoubles==null) return null;
+        if (theDoubles == null) return null;
         float[] floatArray = new float[theDoubles.length];
-        for (int i=0; i<theDoubles.length; i++) floatArray[i] = (float) theDoubles[i];
+        for (int i = 0; i < theDoubles.length; i++) floatArray[i] = (float) theDoubles[i];
         return floatArray;
     }
 
@@ -334,7 +337,7 @@ public class ArrayUtils {
      */
     public static int indexOf(Object[] anArray, Object anObj)
     {
-        for (int i=0, iMax=anArray.length; i<iMax; i++)
+        for (int i = 0, iMax = anArray.length; i < iMax; i++)
             if (anArray[i].equals(anObj))
                 return i;
         return -1;
@@ -353,7 +356,7 @@ public class ArrayUtils {
      */
     public static <T> int indexOfId(T[] anArray, T aValue)
     {
-        for (int i=0, iMax=length(anArray); i<iMax; i++)
+        for (int i = 0, iMax = length(anArray); i < iMax; i++)
             if (anArray[i] == aValue)
                 return i;
         return -1;
@@ -368,23 +371,11 @@ public class ArrayUtils {
     }
 
     /**
-     * Returns a copy of given range of given array (this method is in Java 6 Arrays class).
-     */
-    public static int[] copyOfRange(int[] anArray, int from, int to)
-    {
-        int newLength = to - from;
-        if (newLength < 0) throw new IllegalArgumentException(from + " > " + to);
-        int[] copy = new int[newLength];
-        System.arraycopy(anArray, from, copy, 0, Math.min(anArray.length - from, newLength));
-        return copy;
-    }
-
-    /**
      * Reverse and array.
      */
     public static void reverse(Object[] anArray)
     {
-        for (int i=0, j=anArray.length-1; i<j; i++, j--) {
+        for (int i = 0, j = anArray.length - 1; i < j; i++, j--) {
             Object temp = anArray[i];
             anArray[i] = anArray[j];
             anArray[j] = temp;
@@ -392,38 +383,11 @@ public class ArrayUtils {
     }
 
     /**
-     * Returns a filtered array for given original and Predicate (or original if all satisfy).
+     * Returns a filtered array for given original and Predicate.
      */
     public static <T> T[] filter(T[] anArray, Predicate<? super T> aPredicate)
     {
-        T[] array = anArray;
-        int length = 0;
-
-        // Iterate over array items until one fails
-        for (int i = 0; i < anArray.length; i++) {
-            T item = anArray[i];
-            if (!aPredicate.test(item)) {
-                array = anArray.clone();
-                length = i;
-                break;
-            }
-        }
-
-        // If one failed, iterate over remaining and copy over
-        if (array != anArray) {
-
-            for (int i = length + 1; i < anArray.length; i++) {
-                T item = anArray[i];
-                if (aPredicate.test(item)) {
-                    array[length++] = item;
-                }
-            }
-
-            // Trim array
-            array = Arrays.copyOf(array, length);
-        }
-
-        // Return array
-        return array;
+        Stream<T> filteredStream = Stream.of(anArray).filter(aPredicate);
+        return filteredStream.toArray(size -> (T[]) Array.newInstance(anArray.getClass().getComponentType(), size));
     }
 }
