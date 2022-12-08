e10a9e3375d7b4bf9b9b439916cb39602e25e782
Jeff Martin <jeff@reportmill.com>
2022-12-08 14:27:11 -0600

    Slight fix

--- src/snap/viewx/ColorDock.java
+++ src/snap/viewx/ColorDock.java
@@ -3,13 +3,11 @@
  */
 package snap.viewx;
 import java.util.*;
-
 import snap.geom.Insets;
 import snap.geom.Point;
 import snap.geom.Rect;
 import snap.gfx.*;
 import snap.util.Prefs;
-import snap.util.SnapUtils;
 import snap.util.StringUtils;
 import snap.view.*;
 
@@ -19,7 +17,7 @@ import snap.view.*;
 public class ColorDock extends View {
 
     // A list of colors to show in dock
-    private List <Color>  _colors = new ArrayList();
+    private List<Color>  _colors = new ArrayList<>();
     
     // Whether dock colors are saved to prefs
     private boolean  _persist;
@@ -37,7 +35,7 @@ public class ColorDock extends View {
     private boolean  _dragging;
     
     // The size of the individual swatches
-    private static int      SWATCH_SIZE = 13;
+    private static int  SWATCH_SIZE = 13;
     
     // The border for color dock
     private static final Border  COLOR_DOCK_BORDER = Border.createLoweredBevelBorder();
@@ -63,7 +61,7 @@ public class ColorDock extends View {
     public Color getColor()
     {
         Swatch swatch = getSelSwatch();
-        return swatch!=null? swatch.getColor() : Color.WHITE;
+        return swatch != null? swatch.getColor() : Color.WHITE;
     }
 
     /**
@@ -71,7 +69,9 @@ public class ColorDock extends View {
      */
     public void setColor(Color aColor)
     {
-        Swatch swatch = getSelSwatch(); if (swatch==null) return;
+        Swatch swatch = getSelSwatch();
+        if (swatch == null)
+            return;
         swatch.setColor(aColor);
     }
 
@@ -81,7 +81,7 @@ public class ColorDock extends View {
     public Color getColor(int anIndex)
     {
         // If index beyond range, return white
-        if (anIndex>=_colors.size())
+        if (anIndex >= _colors.size())
             return Color.WHITE;
 
         // Return color at given index
@@ -94,14 +94,14 @@ public class ColorDock extends View {
     public void setColor(Color aColor, int anIndex)
     {
         // If beyond 1000, just bail
-        if (anIndex>1000) return;
+        if (anIndex > 1000) return;
 
         // Fill list to index
-        while(anIndex>=_colors.size())
+        while(anIndex >= _colors.size())
             _colors.add(Color.WHITE);
 
         // Set value at index and repaint
-        _colors.set(anIndex, aColor!=null? aColor : Color.WHITE);
+        _colors.set(anIndex, aColor != null ? aColor : Color.WHITE);
         repaint();
 
         // If Persist, save
@@ -112,10 +112,10 @@ public class ColorDock extends View {
     /**
      * Sets the colors.
      */
-    public void setColors(List <? extends Color> theColors)
+    public void setColors(List<? extends Color> theColors)
     {
         resetColors();
-        for (int i=0, iMax=theColors.size(); i<iMax; i++)
+        for (int i = 0, iMax = theColors.size(); i < iMax; i++)
             setColor(theColors.get(i), i);
     }
 
@@ -124,8 +124,8 @@ public class ColorDock extends View {
      */
     public Color getColor(int aRow, int aCol)
     {
-        int cc = getColCount();
-        int index = aRow*cc + aCol;
+        int colCount = getColCount();
+        int index = aRow * colCount + aCol;
         return getColor(index);
     }
 
@@ -135,9 +135,10 @@ public class ColorDock extends View {
     public void setColor(int aRow, int aCol, Color aColor)
     {
         // Get index and set color (the whole row/col thing was (is) so bogus)
-        int cc = getColCount();
-        if (cc==0) cc = 20;
-        int index = aRow*cc + aCol;
+        int colCount = getColCount();
+        if (colCount == 0)
+            colCount = 20;
+        int index = aRow*colCount + aCol;
         setColor(aColor, index);
     }
 
@@ -152,7 +153,8 @@ public class ColorDock extends View {
     public void setPersistent(boolean aFlag)
     {
         _persist = aFlag;
-        if (_persist) readFromPrefs(getName());
+        if (_persist)
+            readFromPrefs(getName());
     }
 
     /**
@@ -160,7 +162,8 @@ public class ColorDock extends View {
      */
     public Swatch getSwatch(int anIndex)
     {
-        int row = anIndex/getColCount(), col = anIndex%getColCount();
+        int row = anIndex / getColCount();
+        int col = anIndex % getColCount();
         return getSwatch(row, col);
     }
 
@@ -180,8 +183,8 @@ public class ColorDock extends View {
     public Swatch getSwatchAt(double aX, double aY)
     {
         Insets ins = getInsetsAll();
-        int row = (int)((aY - ins.top)/SWATCH_SIZE);
-        int col = (int)((aX - ins.left)/SWATCH_SIZE);
+        int row = (int) ((aY - ins.top) / SWATCH_SIZE);
+        int col = (int) ((aX - ins.left) / SWATCH_SIZE);
         return getSwatch(row,col);
     }
 
@@ -191,7 +194,7 @@ public class ColorDock extends View {
     public int getRowCount()
     {
         int height = (int)Math.round(getHeight() - getInsetsAll().getHeight());
-        return height/SWATCH_SIZE + (height%SWATCH_SIZE !=0 ? 1 : 0);
+        return height / SWATCH_SIZE + (height % SWATCH_SIZE != 0 ? 1 : 0);
     }
 
     /**
@@ -200,13 +203,13 @@ public class ColorDock extends View {
     public int getColCount()
     {
         int width = (int)Math.round(getWidth() - getInsetsAll().getWidth());
-        return width/SWATCH_SIZE;
+        return width / SWATCH_SIZE;
     }
 
     /**
      * Returns the total number of visible swatches.
      */
-    public int getSwatchCount()  { return getRowCount()*getColCount(); }
+    public int getSwatchCount()  { return getRowCount() * getColCount(); }
 
     /**
      * Returns the selected swatch.
@@ -218,7 +221,7 @@ public class ColorDock extends View {
      */
     public void setSelSwatch(Swatch aSwatch)
     {
-        if (SnapUtils.equals(aSwatch,_selSwatch)) return;
+        if (Objects.equals(aSwatch, _selSwatch)) return;
         _selSwatch = aSwatch;
         repaint();
     }
@@ -226,7 +229,7 @@ public class ColorDock extends View {
     /**
      * Returns the selected swatch index.
      */
-    public int getSelIndex()  { return _selSwatch!=null? _selSwatch.getIndex() : -1; }
+    public int getSelIndex()  { return _selSwatch != null? _selSwatch.getIndex() : -1; }
 
     /**
      * Resets the colors in colordock to white.
@@ -271,16 +274,18 @@ public class ColorDock extends View {
         double height = bounds.height - ins.getHeight();
 
         // Get swatch size
-        int swatchW = SWATCH_SIZE, swatchH = SWATCH_SIZE;
+        int swatchW = SWATCH_SIZE;
+        int swatchH = SWATCH_SIZE;
 
         // Fill background to white and clip
         aPntr.setColor(Color.WHITE); aPntr.fillRect(ins.left, ins.top, width, height);
         aPntr.clipRect(ins.left, ins.top, width, height);
 
         // Make as many rows & columns as will fit, and fill any that are present in the sparse array.
-        int nrows = getRowCount(), ncols = getColCount();
-        for (int row=0; row<nrows; ++row) {
-            for (int col=0; col<ncols; ++col) {
+        int nrows = getRowCount();
+        int ncols = getColCount();
+        for (int row = 0; row < nrows; ++row) {
+            for (int col = 0; col < ncols; ++col) {
                 Color color = getColor(row, col);
                 ColorWell.paintSwatch(aPntr, color, ins.left+col*swatchW+2, ins.top+row*swatchH+2, swatchW-3, swatchH-3);
             }
@@ -288,18 +293,25 @@ public class ColorDock extends View {
 
         // Draw grid between swatches
         aPntr.setColor(Color.LIGHTGRAY);
-        for (int row=0; row<=nrows; row++) aPntr.drawLine(ins.left, ins.top+row*swatchH, ins.left+width,ins.top+row*swatchH);
-        for (int col=0; col<=ncols; col++) aPntr.drawLine(ins.left+col*swatchW, ins.top,ins.left+col*swatchW,ins.top+height);
+        for (int row = 0; row <= nrows; row++)
+            aPntr.drawLine(ins.left, ins.top+row*swatchH, ins.left+width,ins.top+row*swatchH);
+        for (int col = 0; col <= ncols; col++)
+            aPntr.drawLine(ins.left+col*swatchW, ins.top,ins.left+col*swatchW,ins.top+height);
 
         // If selected swatch or drag point, highlight
-        if (getSelSwatch()!=null || _dragPoint!=null) {
-            Swatch swatch = _dragPoint!=null? getSwatchAt(_dragPoint) : getSelSwatch();
-            int row = swatch.getRow(), col = swatch.getCol();
-            double x = ins.left + col*swatchW, y = ins.top + row*swatchH;
+        if (getSelSwatch() != null || _dragPoint != null) {
+            Swatch swatch = _dragPoint != null ? getSwatchAt(_dragPoint) : getSelSwatch();
+            int row = swatch.getRow();
+            int col = swatch.getCol();
+            double x = ins.left + col * swatchW;
+            double y = ins.top + row * swatchH;
 
             // Draw red rect and redraw smaller swatch
-            aPntr.setColor(Color.RED); aPntr.drawRect(x,y,swatchW,swatchH); aPntr.drawRect(x+1,y+1,swatchW-2,swatchH-2);
-            aPntr.setColor(Color.WHITE); aPntr.drawRect(x+2,y+2,swatchW-4,swatchH-4);
+            aPntr.setColor(Color.RED);
+            aPntr.drawRect(x, y, swatchW, swatchH);
+            aPntr.drawRect(x + 1,y + 1,swatchW - 2,swatchH - 2);
+            aPntr.setColor(Color.WHITE);
+            aPntr.drawRect(x + 2,y + 2,swatchW - 4,swatchH - 4);
         }
     }
 
@@ -354,8 +366,8 @@ public class ColorDock extends View {
     protected void colorDropped(ViewEvent anEvent)
     {
         // Get color from clipboard
-        Clipboard dboard = anEvent.getClipboard();
-        Color color = dboard.getColor();
+        Clipboard clipboard = anEvent.getClipboard();
+        Color color = clipboard.getColor();
 
         // Get swatch at point, set color and select swatch
         Swatch swatch = getSwatchAt(anEvent.getPoint());
@@ -374,17 +386,20 @@ public class ColorDock extends View {
     {
         // Get color and create image
         Color color = getColor();
-        Image image = Image.get(14,14,true); Painter pntr = image.getPainter();
-        ColorWell.paintSwatch(pntr,color,0,0,14,14);
-        pntr.setColor(Color.BLACK); pntr.drawRect(0,0,14-1,14-1); pntr.flush();
+        Image image = Image.get(14,14,true);
+        Painter pntr = image.getPainter();
+        ColorWell.paintSwatch(pntr, color,0,0,14,14);
+        pntr.setColor(Color.BLACK);
+        pntr.drawRect(0,0,14-1,14-1);
+        pntr.flush();
 
         // Get clipboard and add color+image
-        Clipboard cboard = anEvent.getClipboard();
-        cboard.addData(color);
-        cboard.setDragImage(image);
+        Clipboard clipboard = anEvent.getClipboard();
+        clipboard.addData(color);
+        clipboard.setDragImage(image);
 
         // Start drag
-        cboard.startDrag(); _dragging = true;
+        clipboard.startDrag(); _dragging = true;
     }
 
     /**
@@ -394,7 +409,9 @@ public class ColorDock extends View {
     {
         // Get color from hidden ColorWell and set in current swatch
         Color color = _colorWell.getColor();
-        Swatch swatch = getSelSwatch(); if (swatch==null) return;
+        Swatch swatch = getSelSwatch();
+        if (swatch == null)
+            return;
         swatch.setColor(color);
 
         // Fire action event
@@ -411,12 +428,13 @@ public class ColorDock extends View {
         Prefs prefs = Prefs.getDefaultPrefs().getChild(aName);
 
         // Get color, rgb value, key, then if not white put value, otherwise remove
-        Color c = getColor(anIndex); // Legacy: c = getColor(aRow, aCol);
-        int rgb = c.getRGBA();
+        Color color = getColor(anIndex); // Legacy: c = getColor(aRow, aCol);
+        int rgb = color.getRGBA();
 
         // Get key, if not white put value, otherwise remove
         String key = String.valueOf(anIndex); // Legacy: key = aRow + "," + aCol;
-        if (rgb!=0xFFFFFFFF) prefs.setValue(key, rgb);
+        if (rgb != 0xFFFFFFFF)
+            prefs.setValue(key, rgb);
         else prefs.remove(key);
     }
 
@@ -430,7 +448,7 @@ public class ColorDock extends View {
 
         // Get named node and node keys and iterate over keys
         Prefs prefs = Prefs.getDefaultPrefs().getChild(aName);
-        String keys[] = prefs.getKeys();
+        String[] keys = prefs.getKeys();
         for (String key : keys) {
 
             // Get color for key
@@ -439,7 +457,8 @@ public class ColorDock extends View {
 
             // Legacy: If key contains separator, add by row/col
             if (key.indexOf(',')>0) {
-                String rcStr[] = key.split(","); if (rcStr.length<2) continue;
+                String[] rcStr = key.split(",");
+                if (rcStr.length < 2) continue;
                 int row = StringUtils.intValue(rcStr[0]);
                 int col = StringUtils.intValue(rcStr[1]);
                 setColor(row, col, color);
@@ -456,10 +475,14 @@ public class ColorDock extends View {
     /**
      * Override to set Selected to false when hidden.
      */
+    @Override
     protected void setShowing(boolean aValue)
     {
-        if (aValue==isShowing()) return;
+        // Do normal version
+        if (aValue == isShowing()) return;
         super.setShowing(aValue);
+
+        // Reset selected and swatch
         setSelected(false);
         setSelSwatch(null);
     }
@@ -475,10 +498,10 @@ public class ColorDock extends View {
     protected class Swatch {
 
         // The row, column
-        int          _row, _col;
+        protected int  _row, _col;
 
         // The color
-        Color        _color;
+        protected Color  _color;
 
         /** Creates swatch for row, col. */
         public Swatch(int aRow, int aCol)  { _row = aRow; _col = aCol; }
@@ -495,8 +518,8 @@ public class ColorDock extends View {
         /** Returns the color. */
         public Color getColor()
         {
-            if (_color==null) _color = ColorDock.this.getColor(_row,_col);
-            return _color;
+            if (_color != null) return _color;
+            return _color = ColorDock.this.getColor(_row,_col);
         }
 
         /** Sets the color. */
@@ -509,9 +532,10 @@ public class ColorDock extends View {
         /** Standard equals. */
         public boolean equals(Object anObj)
         {
-            if (anObj==this) return true;
-            Swatch other = anObj instanceof Swatch? (Swatch)anObj : null; if (other==null) return false;
-            return _row==other._row && _col==other._col;
+            if (anObj == this) return true;
+            Swatch other = anObj instanceof Swatch? (Swatch)anObj : null;
+            if (other == null) return false;
+            return _row == other._row && _col == other._col;
         }
     }
 }
