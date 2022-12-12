b3c0a0fe08d49aeb81cfe089669387dfbfd15912
Jeff Martin <jeff@reportmill.com>
2022-12-12 15:41:05 -0600

    Moved bindings from snp file to ViewOwner

--- src/snap/viewx/LoginPage.java
+++ src/snap/viewx/LoginPage.java
@@ -12,82 +12,110 @@ import snap.web.WebSite;
 public class LoginPage extends WebPage {
 
     // The site, if running as panel
-    WebSite        _site;
+    private WebSite  _site;
 
-/**
- * Shows a login panel. Returns true is username or password were updated.
- */
-public boolean showPanel(View aView, WebSite aSite)
-{
-    String usr = aSite.getUserName(), pw = aSite.getPassword();
-    _site = aSite;
-    DialogBox dbox = new DialogBox("Login Panel"); dbox.setContent(getUI());
-    boolean confirmed = dbox.showConfirmDialog(aView);
-    if(confirmed) confirmed = !SnapUtils.equals(usr, getName()) || !SnapUtils.equals(pw, getPassword());
-    if(confirmed) ClientUtils.setAccess(getSite(), getName(), getPassword());
-    _site = null;
-    return confirmed;
-}
+    /**
+     * Shows a login panel. Returns true is username or password were updated.
+     */
+    public boolean showPanel(View aView, WebSite aSite)
+    {
+        String usr = aSite.getUserName(), pw = aSite.getPassword();
+        _site = aSite;
+        DialogBox dbox = new DialogBox("Login Panel");
+        dbox.setContent(getUI());
+        boolean confirmed = dbox.showConfirmDialog(aView);
+        if (confirmed) confirmed = !SnapUtils.equals(usr, getName()) || !SnapUtils.equals(pw, getPassword());
+        if (confirmed) ClientUtils.setAccess(getSite(), getName(), getPassword());
+        _site = null;
+        return confirmed;
+    }
 
-/**
- * Show panel for given data source.
- */
-protected void initUI()
-{
-    // Set FirstFocus and SiteText
-    setFirstFocus("NameText");
-    setViewValue("SiteText", "Site: " + getSite().getURLString());
-    
-    // Make LoginButton default
-    getView("LoginButton", Button.class).setDefaultButton(true);
-    
-    // If previous access found, try again
-    if(ClientUtils.setAccess(getSite()))
-        getBrowser().setURL(getURL());
-}
+    /**
+     * Show panel for given data source.
+     */
+    protected void initUI()
+    {
+        // Set FirstFocus and SiteText
+        setFirstFocus("NameText");
+        setViewValue("SiteText", "Site: " + getSite().getURLString());
 
-/**
- * Respond to UI.
- */
-protected void respondUI(ViewEvent anEvent)
-{
-    // Handle LoginButton
-    if(anEvent.equals("LoginButton")) {
-        if(getRememberLogin())
-            ClientUtils.setAccess(getSite(), getName(), getPassword());
-        if(getBrowser()!=null)
+        // Make LoginButton default
+        getView("LoginButton", Button.class).setDefaultButton(true);
+
+        // If previous access found, try again
+        if (ClientUtils.setAccess(getSite()))
             getBrowser().setURL(getURL());
     }
-}
 
-/**
- * Returns the site.
- */
-public WebSite getSite()  { return _site!=null? _site : super.getSite(); }
+    /**
+     * Reset UI.
+     */
+    @Override
+    protected void resetUI()
+    {
+        setViewText("NameText", getName());
+        setViewText("PasswordText", getPassword());
+    }
 
-/**
- * Returns the name.
- */
-public String getName()  { return getSite().getUserName(); }
+    /**
+     * Respond to UI.
+     */
+    protected void respondUI(ViewEvent anEvent)
+    {
+        // Handle LoginButton
+        if (anEvent.equals("LoginButton")) {
+            if (getRememberLogin())
+                ClientUtils.setAccess(getSite(), getName(), getPassword());
+            if (getBrowser() != null)
+                getBrowser().setURL(getURL());
+        }
+    }
 
-/**
- * Sets the name.
- */
-public void setName(String aString)  { getSite().setUserName(aString); }
+    /**
+     * Returns the site.
+     */
+    public WebSite getSite()
+    {
+        return _site != null ? _site : super.getSite();
+    }
 
-/**
- * Returns the password.
- */
-public String getPassword()  { return getSite().getPassword(); }
+    /**
+     * Returns the name.
+     */
+    public String getName()
+    {
+        return getSite().getUserName();
+    }
 
-/**
- * Sets the password.
- */
-public void setPassword(String aString)  { getSite().setPassword(aString); }
+    /**
+     * Sets the name.
+     */
+    public void setName(String aString)
+    {
+        getSite().setUserName(aString);
+    }
 
-/**
- * Returns whether to remember username/password.
- */
-public boolean getRememberLogin()  { return getViewBoolValue("RememberLoginCheckBox"); }
+    /**
+     * Returns the password.
+     */
+    public String getPassword()
+    {
+        return getSite().getPassword();
+    }
 
+    /**
+     * Sets the password.
+     */
+    public void setPassword(String aString)
+    {
+        getSite().setPassword(aString);
+    }
+
+    /**
+     * Returns whether to remember username/password.
+     */
+    public boolean getRememberLogin()
+    {
+        return getViewBoolValue("RememberLoginCheckBox");
+    }
 }
