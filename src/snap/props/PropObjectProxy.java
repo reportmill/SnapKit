2264db1ff12f7cf188f21047a8468bb1c44df115
Jeff Martin <jeff@reportmill.com>
2022-12-16 12:18:47 -0600

    Some archival work

--- src/snap/props/PropObjectProxy.java
+++ src/snap/props/PropObjectProxy.java
@@ -32,4 +32,54 @@ public abstract class PropObjectProxy<T> extends PropObject {
     {
         _real = aReal;
     }
+
+    /**
+     * Override to forward to Real if set.
+     */
+    @Override
+    protected void initProps(PropSet aPropSet)
+    {
+        // If Real is set and is PropObject, use its props (should probably add copies).
+        if (_real instanceof PropObject) {
+            PropObject realPropObj = (PropObject) _real;
+            Prop[] dataSetProps = realPropObj.getPropSet().getProps();
+            for (Prop prop : dataSetProps)
+                aPropSet.addProp(prop);
+        }
+
+        // Do normal version
+        else super.initProps(aPropSet);
+    }
+
+    /**
+     * Override to forward to Real.
+     */
+    @Override
+    public Prop[] getPropsForArchivalExtra()
+    {
+        // If Real is set and is PropObject, forward on.
+        if (_real instanceof PropObject) {
+            PropObject realPropObj = (PropObject) _real;
+            return realPropObj.getPropsForArchivalExtra();
+        }
+
+        // Do normal version
+        return super.getPropsForArchivalExtra();
+    }
+
+    /**
+     * Override to forward to Real.
+     */
+    @Override
+    public Object getPropValue(String aPropName)
+    {
+        // If Real is set and is PropObject, forward on.
+        if (_real instanceof PropObject) {
+            PropObject realPropObj = (PropObject) _real;
+            return realPropObj.getPropValue(aPropName);
+        }
+
+        // Do normal version
+        return super.getPropValue(aPropName);
+    }
 }
