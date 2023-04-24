b64364890ce6a874d9c868d7385e17d4f4c0e14a
Jeff Martin <jeff@reportmill.com>
2023-04-24 17:43:37 -0500

    Some refactoring

--- src/snap/text/SpellCheck.java
+++ src/snap/text/SpellCheck.java
@@ -2,128 +2,116 @@
  * Copyright (c) 2010, ReportMill Software. All rights reserved.
  */
 package snap.text;
-//import com.wintertree.ssce.*;
 import java.util.*;
 
 /**
- * This class provides provides generic spell check API ideal for RM's needs. The abstraction should let us
- * plug in any spell check technology.
+ * This class provides generic spell check API to allow support for any spell check technology.
  */
 public class SpellCheck {
-    
-//    // An object that can pull a word from a string
-//    static StringWordParser _parser = new StringWordParser(true);
-//
-//    // An object that can find the next misspelled word from a parser
-//    static SpellingSession  _speller;
-    
-/**
- * Returns the first misspelled word in a given string starting at the given index (or null or no misspelled words).
- */    
-public static Word getMisspelledWord(String aString, int anIndex)
-{
-//    // If given index is already beyond string bounds, just return null
-//    if(anIndex>=aString.length()) return null;
-//
-//    // Configure word parser for given string and index
-//    _parser.setText(aString);
-//    _parser.setCursor(anIndex);
-//
-//    // Get Speller, create new Word and declare variable for check result
-//    SpellingSession speller = getSpeller();
-//    Word word = new Word();
-//    int cr;
-//
-//    // Iterate over words
-//    while(((cr=speller.check(_parser, word._otherWord)) & SpellingSession.END_OF_TEXT_RSLT) == 0) {
-//
-//        // If misspelled, configure word and return
-//        if((cr & SpellingSession.MISSPELLED_WORD_RSLT) != 0) {
-//            word._start = _parser.getCursor();
-//            word._string = _parser.nextWord();
-//            word._end = word._start + word._string.length();
-//            return word;
-//        }
-//
-//        // Find next word
-//        _parser.nextWord();
-//    }
-    
-    // If no more misspelled words, return null
-    return null;
-}
 
-///**
-// * Creates and returns a speller.
-// */
-//private static SpellingSession getSpeller()
-//{
-//    // If speller not created, create and set
-//    if(_speller==null) try {
-//
-//        // Set license key
-//        LicenseKey.setKey(0x80CD5259);
-//
-//        // Get lexicons
-//        InputStream is1 = SpellCheck.class.getResourceAsStream("/com/wintertree/ssceam.tlx");
-//        StreamTextLexicon lex1 = new StreamTextLexicon(is1);
-//        InputStream is2 = SpellCheck.class.getResourceAsStream("/com/wintertree/ssceam2.clx");
-//        CompressedLexicon lex2 = new CompressedLexicon(is2);
-//        InputStream is3 = SpellCheck.class.getResourceAsStream("/com/wintertree/tech.tlx");
-//        StreamTextLexicon lex3 = new StreamTextLexicon(is3);
-//
-//        // Get speller
-//        _speller = new SpellingSession();
-//
-//        // Set lexicons
-//        _speller.setLexicons(new Lexicon[] { lex1, lex2, lex3 });
-//
-//    // Catch exceptions
-//    } catch(Exception e) { e.printStackTrace(); }
-//
-//    // Return speller
-//    return _speller;
-//}
+    // The shared SpellCheck
+    private static SpellCheck _shared;
 
-/**
- * An inner class to represent a misspelled word.
- */
-public static class Word {
-    
-    // The misspelled word
-    String         _string;
-    
-    // The start/end index from original text
-    int            _start, _end;
-    
-    // I can't figure this out, but needed for WinterTree SpellingSession check()
-    StringBuffer   _otherWord = new StringBuffer();
-    
-    /** Returns the string. */
-    public String getString() { return _string; }
-    
-    /** Returns the start index. */
-    public int getStart() { return _start; }
-    
-    /** Returns the end index. */
-    public int getEnd() { return _end; }
-    
-    /** Returns a list of suggestions. */
-    public List <String> getSuggestions()
+    // The shared SpellCheck
+    private static Class<? extends SpellCheck> _sharedClass;
+
+    /**
+     * Constructor.
+     */
+    public SpellCheck()
     {
-        // Get speller, create suggestion set and fill with suggestions
-//        SpellingSession speller = getSpeller();
-//        SuggestionSet set = new SuggestionSet(8);
-//        speller.suggest(getString(), SpellingSession.MAX_SUGGEST_DEPTH, new EnglishPhoneticComparator(), set);
-        
-        // Create list and fill from suggestion set and return
-        List list = new ArrayList();
-//        for(Enumeration e=set.words(); e.hasMoreElements();) list.add(e.nextElement());
-        return list;
+        super();
     }
-    
-    /** Returns string representation of word. */
-    public String toString() { return "Misspelled word: " + (_string!=null? _string : ""); }
-}
-    
+
+    /**
+     * Returns the next misspelled word in given string.
+     */
+    public Word getNextMisspelledWord(String aString, int anIndex)
+    {
+        return null;
+    }
+
+    /**
+     * Returns the suggestions for given word.
+     */
+    protected List<String> getSuggestionsForWord(Word aWord)
+    {
+        return Collections.EMPTY_LIST;
+    }
+
+    /**
+     * Returns the first misspelled word in a given string starting at the given index (or null or no misspelled words).
+     */
+    public static Word getMisspelledWord(String aString, int anIndex)
+    {
+        SpellCheck spellCheck = getShared();
+        return spellCheck.getNextMisspelledWord(aString, anIndex);
+    }
+
+    /**
+     * Returns the shared spell checker.
+     */
+    public static SpellCheck getShared()
+    {
+        if (_shared != null) return _shared;
+
+        // Create new instance
+        Class<? extends SpellCheck> sharedClass = _sharedClass != null ? _sharedClass : SpellCheck.class;
+        SpellCheck shared;
+        try { shared = sharedClass.getConstructor().newInstance(); }
+        catch (Exception e) { throw new RuntimeException(e); }
+
+        // Return
+        return _shared = shared;
+    }
+
+    /**
+     * Returns the shared spell checker.
+     */
+    public static void setSharedClass(Class<? extends SpellCheck> aClass)
+    {
+        _sharedClass = aClass;
+        _shared = null;
+    }
+
+    /**
+     * An inner class to represent a misspelled word.
+     */
+    public static class Word {
+
+        // The misspelled word
+        protected String _string;
+
+        // The start/end index from original text
+        protected int _start, _end;
+
+        /**
+         * Constructor for given word at index.
+         */
+        public Word(String aString, int aStart)
+        {
+            _string = aString;
+            _start = aStart;
+            _end = aStart + aString.length();
+        }
+
+        /** Returns the string. */
+        public String getString() { return _string; }
+
+        /** Returns the start index. */
+        public int getStart() { return _start; }
+
+        /** Returns the end index. */
+        public int getEnd() { return _end; }
+
+        /** Returns a list of suggestions. */
+        public List<String> getSuggestions()
+        {
+            SpellCheck spellCheck = getShared();
+            return spellCheck.getSuggestionsForWord(this);
+        }
+
+        /** Returns string representation of word. */
+        public String toString() { return "Misspelled word: " + (_string != null ? _string : ""); }
+    }
 }
--- src/snap/viewx/SpellCheckPanel.java
+++ src/snap/viewx/SpellCheckPanel.java
@@ -10,124 +10,170 @@ import snap.view.*;
  * This class uses the SpellCheck class to check spelling.
  */
 public abstract class SpellCheckPanel extends ViewOwner {
-    
+
     // The editor
-    View _view;
+    private View _view;
 
     // The current suspected misspelled word
     private SpellCheck.Word _word;
 
-/**
- * Runs the spell check panel.
- */
-public void show(View aView)
-{
-    // Make window visible and find next misspelling
-    getWindow().setSaveName("SpellCheck");
-    getWindow().setVisible(true);
-    findNextMisspelling();
-}
+    /**
+     * Constructor.
+     */
+    public SpellCheckPanel()
+    {
+        super();
+    }
 
-/**
- * Initialize UI.
- */
-protected void initUI()
-{
-    // Configure SuggestionList to watch for MouseClick
-    enableEvents("SuggestionList", MouseRelease);
-}
+    /**
+     * Runs the spell check panel.
+     */
+    public void show(View aView)
+    {
+        // Make window visible
+        WindowView window = getWindow();
+        window.setAlwaysOnTop(true);
+        window.setSaveName("SpellCheck");
+        window.setType(WindowView.TYPE_UTILITY);
+        window.setVisible(true);
 
-/**
- * Reset the UI.
- */
-public void resetUI()
-{
-    // Reset WordText
-    setViewValue("WordText", _word==null? "" : _word.getString());
-    
-    // Reset SuggestionList Items and SelIndex
-    Object items[] = _word!=null? _word.getSuggestions().toArray() : new Object[0];
-    setViewItems("SuggestionList", items);
-    if(items.length>0) setViewSelIndex("SuggestionList", 0);
-}
-
-/**
- * Handles okay button.
- */
-public void respondUI(ViewEvent anEvent)
-{
-    // Handle CloseButton: Just close the window and return
-    if(anEvent.equals("CloseButton")) {
-        setWindowVisible(false); return; }
-    
-    // Handle IgnoreButton: find and hilight next misspelled word
-    if(anEvent.equals("FindNextButton"))
+        // Find next misspelling
         findNextMisspelling();
-    
-    // Handle CorrectButton
-    else if(anEvent.equals("CorrectButton"))
-        doCorrection();
-    
-    // Handle SuggestionList
-    else if(anEvent.equals("SuggestionList") && anEvent.getClickCount()>1)
-        doCorrection();
-}
-
-/**
- * Find next misspelling.
- */
-public void findNextMisspelling()
-{
-    // Get text editor and text
-    TextEditor textEditor = getTextEditor();
-    String text = getText(); if(text==null) return;
-    
-    // Get next misspelled word
-    _word = SpellCheck.getMisspelledWord(text, textEditor.getSelEnd());
-    
-    // If not null, select word
-    if(_word!=null)
-        textEditor.setSel(_word.getStart(), _word.getEnd());
-    
-    // Otherwise, set selection to end and beep
-    else {
-        textEditor.setSel(textEditor.length());
-        beep();
     }
 
-    // Reset panel
-    resetLater();
-}
-
-/**
- * Do correction.
- */
-public void doCorrection()
-{
-    // Get main editor and text editor and text
-    TextEditor textEditor = getTextEditor();
-    
-    // Get suggested word from list
-    String correctWord = getViewStringValue("SuggestionList");
-    
-    // Replace in text editor
-    if(_word!=null && correctWord!=null) {
-        textEditor.setSel(_word.getStart(), _word.getEnd());
-        textEditor.replaceChars(correctWord);
+    /**
+     * Initialize UI.
+     */
+    protected void initUI()
+    {
+        // Configure SuggestionListArea to watch for MouseClick
+        ListView<?> suggestionListView = getView("SuggestionList", ListView.class);
+        ListArea<?> suggestionListArea = suggestionListView.getListArea();
+        suggestionListArea.setName("SuggestionListArea");
+        enableEvents(suggestionListArea, MouseRelease);
     }
-    
-    // Find next misspelling
-    findNextMisspelling();
-}
 
-/**
- * Returns the text that this spell check panel should be checking.
- */
-protected abstract String getText();
+    /**
+     * Reset the UI.
+     */
+    public void resetUI()
+    {
+        // Reset WordText
+        setViewValue("WordText", _word == null ? "" : _word.getString());
 
-/**
- * Returns the TextEditor.
- */
-protected abstract TextEditor getTextEditor();
+        // Reset SuggestionList Items and SelIndex
+        Object[] items = _word != null ? _word.getSuggestions().toArray() : new Object[0];
+        setViewItems("SuggestionList", items);
+        if (items.length > 0)
+            setViewSelIndex("SuggestionList", 0);
+    }
 
+    /**
+     * Handles okay button.
+     */
+    public void respondUI(ViewEvent anEvent)
+    {
+        // Handle CloseButton: Just close the window and return
+        if (anEvent.equals("CloseButton")) {
+            setWindowVisible(false);
+            return;
+        }
+
+        // Handle IgnoreButton: find and hilight next misspelled word
+        if (anEvent.equals("FindNextButton"))
+            findNextMisspelling();
+
+        // Handle CorrectButton
+        else if (anEvent.equals("CorrectButton"))
+            doCorrection();
+
+        // Handle SuggestionList
+        else if (anEvent.equals("SuggestionListArea") && anEvent.getClickCount() > 1)
+            doCorrection();
+    }
+
+    /**
+     * Find next misspelling.
+     */
+    public void findNextMisspelling()
+    {
+        // Get text editor and text
+        String textString = getTextString();
+        if (textString == null)
+            return;
+        int textSelEnd = getSelEnd();
+
+        // Get next misspelled word
+        _word = SpellCheck.getMisspelledWord(textString, textSelEnd);
+
+        // If not null, select word
+        if (_word != null)
+            setSel(_word.getStart(), _word.getEnd());
+
+            // Otherwise, set selection to end and beep
+        else {
+            setSel(getTextLength());
+            beep();
+        }
+
+        // Reset panel
+        resetLater();
+    }
+
+    /**
+     * Do correction.
+     */
+    public void doCorrection()
+    {
+        // Get suggested word from list
+        String correctWord = getViewStringValue("SuggestionList");
+
+        // Replace in text editor
+        if (_word != null && correctWord != null) {
+            setSel(_word.getStart(), _word.getEnd());
+            replaceChars(correctWord);
+        }
+
+        // Find next misspelling
+        findNextMisspelling();
+    }
+
+    /**
+     * Returns the text that this spell check panel should be checking.
+     */
+    protected abstract String getTextString();
+
+    /**
+     * Returns the TextEditor.
+     */
+    protected TextEditor getTextEditor()  { return null; }
+
+    protected int getTextLength()
+    {
+        TextEditor textEditor = getTextEditor();
+        return textEditor.length();
+    }
+
+    protected int getSelEnd()
+    {
+        TextEditor textEditor = getTextEditor();
+        return textEditor.getSelEnd();
+    }
+
+    protected void setSel(int aStart)
+    {
+        setSel(aStart, aStart);
+    }
+
+    protected void setSel(int aStart, int anEnd)
+    {
+        TextEditor textEditor = getTextEditor();
+        textEditor.setSel(aStart, anEnd);
+    }
+
+    protected void replaceChars(String aString)
+    {
+        TextEditor textEditor = getTextEditor();
+        textEditor.replaceChars(aString);
+    }
 }
