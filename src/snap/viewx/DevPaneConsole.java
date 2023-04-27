6cc8721a9740c8c80aee0758bbaffa58452fd98a
Jeff Martin <jeff@reportmill.com>
2023-04-27 14:20:47 -0500

    DevPaneConsole: Added support to show std out/err

--- src/snap/viewx/DevPaneConsole.java
+++ src/snap/viewx/DevPaneConsole.java
@@ -1,4 +1,5 @@
 package snap.viewx;
+import snap.gfx.Color;
 import snap.gfx.Font;
 import snap.text.TextLineStyle;
 import snap.text.TextStyle;
@@ -8,6 +9,7 @@ import snap.view.BoxView;
 import snap.view.ScrollView;
 import snap.view.View;
 import snap.view.ViewOwner;
+import java.io.PrintStream;
 
 /**
  * A DevPane to show the console.
@@ -15,11 +17,25 @@ import snap.view.ViewOwner;
 public class DevPaneConsole extends ViewOwner {
 
     // The ConsoleView
-    private ConsoleView  _consoleView;
+    private static ConsoleView  _consoleView;
 
     // A counter of current input
     private int  _inputIndex = 1;
 
+    // PrintStreams to capture StdOut, StdErr
+    private static ProxyPrintStream _stdOut, _stdErr;
+
+    /**
+     * Constructor.
+     */
+    public DevPaneConsole()
+    {
+        super();
+    }
+
+    /**
+     * Create UI.
+     */
     @Override
     protected View createUI()
     {
@@ -37,9 +53,31 @@ public class DevPaneConsole extends ViewOwner {
     }
 
     /**
+     * Initialize UI.
+     */
+    @Override
+    protected void initUI()
+    {
+        if (_stdOut != null) {
+            _consoleView.addChars(_stdOut._sb.toString(), _stdOut._textStyle);
+            _consoleView.addChars(_stdErr._sb.toString(), _stdErr._textStyle);
+        }
+    }
+
+    /**
+     * Tell DevPaneConsole to start watching StdOut, StdErr.
+     */
+    public static void captureSystemOutAndErr()
+    {
+        // Set System out/err to catch console output
+        System.setOut(_stdOut = new ProxyPrintStream(System.out));
+        System.setErr(_stdErr = new ProxyPrintStream(System.err));
+    }
+
+    /**
      * A ConsoleView subclass that works with scanner.
      */
-    public class DevConsoleView extends ConsoleView {
+    private class DevConsoleView extends ConsoleView {
 
         /**
          * Constructor.
@@ -79,4 +117,62 @@ public class DevPaneConsole extends ViewOwner {
         }
     }
 
+    /**
+     * A PrintStream to stand in for System.out and System.err.
+     */
+    private static class ProxyPrintStream extends PrintStream {
+
+        // A buffer to store output when console not active
+        private StringBuilder _sb = new StringBuilder();
+
+        // TextStyle
+        private TextStyle _textStyle;
+
+        /**
+         * Constructor.
+         */
+        public ProxyPrintStream(PrintStream printStream)
+        {
+            super(printStream);
+            if (printStream == System.err)
+                _textStyle = TextStyle.DEFAULT.copyFor(Color.RED);
+        }
+
+        /**
+         * Override to send to ScanView.
+         */
+        public void write(int b)
+        {
+            // Do normal version
+            super.write(b);
+
+            // Write char to console
+            String str = String.valueOf(Character.valueOf((char) b));
+            writeString(str);
+        }
+
+        /**
+         * Override to send to ScanView.
+         */
+        public void write(byte[] buf, int off, int len)
+        {
+            // Do normal version
+            super.write(buf, off, len);
+
+            // Write buff to console
+            String str = new String(buf, off, len);
+            writeString(str);
+        }
+
+        /**
+         * Override to send to ScanView.
+         */
+        public void writeString(String aString)
+        {
+            // Write buff to console
+            if (_consoleView != null)
+                _consoleView.addChars(aString, _textStyle);
+            else _sb.append(aString);
+        }
+    }
 }
