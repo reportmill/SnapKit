02331613e978daba1080e85f9b156e3a6bff9a00
Jeff Martin <jeff@reportmill.com>
2023-07-05 15:50:20 -0500

    MenuButton: Some improvements

--- src/snap/view/MenuButton.java
+++ src/snap/view/MenuButton.java
@@ -3,6 +3,7 @@
  */
 package snap.view;
 import java.util.*;
+import snap.geom.HPos;
 import snap.geom.Point;
 import snap.geom.Size;
 import snap.util.*;
@@ -24,11 +25,15 @@ public class MenuButton extends ButtonBase {
     // The items
     private List<MenuItem>  _items = new ArrayList<>();
 
+    // The menu
+    private Menu _menu;
+
     /**
      * Constructor.
      */
     public MenuButton()
     {
+        super();
         setShowArrow(true);
     }
 
@@ -73,7 +78,14 @@ public class MenuButton extends ButtonBase {
         if (aValue) {
             ImageView arrowView = new ImageView(ComboBox.getArrowImage());
             arrowView.setPadding(0, 2, 0, 2);
+            arrowView.setLeanX(HPos.RIGHT);
             setGraphicAfter(arrowView);
+
+            // Configure label
+            Label label = getLabel();
+            label.setGrowWidth(true);
+            label.setAlignX(HPos.CENTER);
+            label.setPadding(2, 2, 2, 2);
         }
 
         // Otherwise, remove
@@ -110,47 +122,84 @@ public class MenuButton extends ButtonBase {
     }
 
     /**
+     * Returns the menu.
+     */
+    public Menu getMenu()
+    {
+        // If already set, just return
+        if (_menu != null) return _menu;
+
+        // Create menu with items
+        Menu menu = new Menu();
+        List<MenuItem> menuItems = getItems();
+        for (MenuItem item : menuItems)
+            menu.addItem(item);
+
+        // Return
+        return _menu = menu;
+    }
+
+    /**
+     * Returns whether menu is showing.
+     */
+    public boolean isMenuShowing()  { return _menu != null; }
+
+    /**
+     * Shows the popup menu.
+     */
+    public void showMenu()
+    {
+        // Get menu with items
+        Menu menu = getMenu();
+
+        // Show menu
+        double menuY = getHeight();
+        menu.show(this, 0, menuY);
+        menu.getPopup().addPropChangeListener(pc -> ViewUtils.runLater(() -> hideMenu()), Showing_Prop);
+    }
+
+    /**
+     * Hides the menu.
+     */
+    public void hideMenu()
+    {
+        if (_menu == null) return;
+        _menu.hide();
+        _menu = null;
+    }
+
+    /**
      * Handle events.
      */
     protected void processEvent(ViewEvent anEvent)
     {
         // Handle MouseEnter
-        if (anEvent.isMouseEnter()) {
+        if (anEvent.isMouseEnter())
             setTargeted(true);
-            repaint();
-        }
 
         // Handle MouseExit
-        else if (anEvent.isMouseExit()) {
+        else if (anEvent.isMouseExit())
             setTargeted(false);
-            repaint();
-        }
 
         // Handle MousePress
         else if (anEvent.isMousePress()) {
             setPressed(false);
             setTargeted(false);
-            fireActionEvent(anEvent);
-            repaint();
+            if (!isMenuShowing())
+                showMenu();
+            else hideMenu();
         }
 
         // Handle MouseRelease
         else if (anEvent.isMouseRelease()) {
+            boolean isPressed = isPressed();
             setPressed(false);
             setTargeted(false);
-            repaint();
-        }
-    }
 
-    /**
-     * Override to show menu.
-     */
-    protected void fireActionEvent(ViewEvent anEvent)
-    {
-        super.fireActionEvent(anEvent);
-        Menu popup = new Menu();
-        for (MenuItem item : getItems()) popup.addItem(item);
-        popup.show(this, 0, getHeight());
+            // Fire action event
+            if (isPressed)
+                fireActionEvent(anEvent);
+        }
     }
 
     /**
@@ -159,7 +208,8 @@ public class MenuButton extends ButtonBase {
     public void setOwner(ViewOwner anOwner)
     {
         super.setOwner(anOwner);
-        for (View child : _items) child.setOwner(anOwner);
+        for (View child : _items)
+            child.setOwner(anOwner);
     }
 
     /**
