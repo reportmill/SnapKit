d0b265e63b35e935c1de08016d4bec99db3beb08
Jeff Martin <jeff@reportmill.com>
2023-12-28 11:04:41 -0600

    TaskMonitor: Made this a real class instead of interface, removed subclasses TaskMonitorPanel: Made this subclass TaskMonitor

--- src/snap/util/TaskMonitor.java
+++ src/snap/util/TaskMonitor.java
@@ -2,88 +2,221 @@
  * Copyright (c) 2010, ReportMill Software. All rights reserved.
  */
 package snap.util;
+import snap.props.PropObject;
 import java.io.*;
+import java.util.Objects;
 
 /**
  * Interface for tracking the progress of arbitrary tasks.
  */
-public interface TaskMonitor {
+public class TaskMonitor extends PropObject {
+
+    // The total number of tasks
+    private int _tasksTotal;
+
+    // The number of tasks completed
+    private int _tasksDone;
+
+    // The total number of task work units
+    private int _taskTotal;
+
+    // The number of task work units completed
+    private int _taskDone;
+
+    // The current task title
+    private String _taskTitle = "First Task";
+
+    // Whether monitor has been cancelled
+    private boolean _cancelled;
+
+    // Whether monitor has finished
+    private boolean _finished;
+
+    // An optional writer to output progress
+    private Writer _writer;
+
+    // Constants for properties
+    public static final String TasksTotal_Prop = "TasksTotal";
+    public static final String TasksDone_Prop = "TasksDone";
+    public static final String TaskTotal_Prop = "TaskTotal";
+    public static final String TaskDone_Prop = "TaskDone";
+    public static final String TaskTitle_Prop = "TaskTitle";
+    public static final String Cancelled_Prop = "Cancelled";
+    public static final String Finished_Prop = "Finished";
+
+    /**
+     * Constructor.
+     */
+    public TaskMonitor()
+    {
+        super();
+    }
+
+    /**
+     * Constructor for given writer.
+     */
+    public TaskMonitor(PrintStream aPrintStream)
+    {
+        super();
+        _writer = new PrintWriter(aPrintStream);
+    }
+
+    /**
+     * Returns the total number of tasks.
+     */
+    public int getTasksTotal()  { return _tasksTotal; }
+
+    /**
+     * Sets the total number of tasks.
+     */
+    protected void setTasksTotal(int aValue)
+    {
+        if (aValue == _tasksTotal) return;
+        firePropChange(TasksTotal_Prop, _tasksTotal, _tasksTotal = aValue);
+    }
+
+    /**
+     * Returns the number of tasks done.
+     */
+    public int getTasksDone()  { return _tasksDone; }
+
+    /**
+     * Sets the number of tasks done.
+     */
+    protected void setTasksDone(int aValue)
+    {
+        if (aValue == _tasksDone) return;
+        firePropChange(TasksDone_Prop, _tasksDone, _tasksDone = aValue);
+    }
+
+    /**
+     * Returns the total number of task work units.
+     */
+    public int getTaskTotal()  { return _taskTotal; }
+
+    /**
+     * Sets the total number of task work units.
+     */
+    protected void setTaskTotal(int aValue)
+    {
+        if (aValue == _taskTotal) return;
+        firePropChange(TaskTotal_Prop, _taskTotal, _taskTotal = aValue);
+    }
+
+    /**
+     * Returns the number of task work units completed.
+     */
+    public int getTaskDone()  { return _taskDone; }
+
+    /**
+     * Sets the number of task work units completed.
+     */
+    protected void setTaskDone(int aValue)
+    {
+        if (aValue == _taskDone) return;
+        firePropChange(TaskDone_Prop, _taskDone, _taskDone = aValue);
+    }
+
+    /**
+     * Returns the current task title.
+     */
+    public String getTaskTitle()  { return _taskTitle; }
+
+    /**
+     * Sets the current task title.
+     */
+    protected void setTaskTitle(String aString)
+    {
+        if (Objects.equals(aString, _taskTitle)) return;
+        firePropChange(TaskTitle_Prop, _taskTitle, _taskTitle = aString);
+    }
 
     /**
      * Advise the monitor of the total number of subtasks (invoke only once).
      */
-    default void startTasks(int aTaskCount)  { }
+    public void startTasks(int aTaskCount)
+    {
+        setTasksTotal(aTaskCount);
+        if (_writer != null)
+            println("StartTasks: " + aTaskCount);
+    }
     
     /**
      * Begin processing a single task.
      */
-    default void beginTask(String aTitle, int theTotalWork)  { }
+    public void beginTask(String aTitle, int theTotalWork)
+    {
+        setTaskTitle(aTitle);
+        setTaskTotal(theTotalWork);
+        setTaskDone(0);
+        if (_writer != null) {
+            String msg = String.format("Begin task %d of %d: %s (%d parts)", _tasksDone + 1, _tasksTotal, aTitle, theTotalWork);
+            println(msg);
+        }
+    }
     
     /**
      * Denote that some work units have been completed.
      */
-    default void updateTask(int theWorkDone)  { }
+    public void updateTask(int theWorkDone)
+    {
+        setTaskDone(_taskDone + theWorkDone);
+        if (_writer != null)
+            println("UpdateTask " + (_tasksDone + 1) + ": " + theWorkDone);
+    }
     
     /**
      * Finish the current task, so the next can begin.
      */
-    default void endTask()  { }
+    public void endTask()
+    {
+        setTasksDone(_tasksDone + 1);
+        setTaskDone(_taskTotal);
+        if (_tasksDone >= _taskTotal)
+            setFinished(true);
+        if (_writer != null)
+            println("EndTask " + _tasksDone);
+    }
     
     /**
-     * Check for user task cancellation.
-     * @return true if the user asked the process to stop working.
+     * Returns whether the user asked the process to stop working.
      */
-    default boolean isCancelled()  { return false; }
+    public boolean isCancelled()  { return _cancelled; }
 
     /**
-     * A simple progress reporter printing on a stream.
+     * Sets whether the user asked the process to stop working.
      */
-    class Text implements TaskMonitor {
-
-        int _tasksDone, _tasksTotal;
-        int _taskDone, _taskTotal;
-        Writer _out;
-
-        /** Initialize a new progress monitor. */
-        public Text(PrintStream aPS)
-        {
-            this(new PrintWriter(aPS));
-        }
-
-        /** Initialize a new progress monitor. */
-        public Text(Writer out)  { _out = out; }
-
-        /** Called to start monitor with number of tasks. */
-        public void startTasks(int aTaskCount)  { _tasksTotal = aTaskCount; }
-
-        /** Called to start task with total work. */
-        public void beginTask(String aTitle, int theTotalWork)
-        {
-            _taskTotal = theTotalWork; _taskDone = 0;
-            print(aTitle + "    ...    ");
-        }
-
-        /** Called to update task work count. */
-        public void updateTask(int theWorkDone)  { _taskTotal += theWorkDone; }
-
-        /** Called to end task work count. */
-        public void endTask()
-        {
-            print("Completed\n");
-            _taskDone = _taskTotal;
-            _tasksDone++;
-        }
-
-        /** Print string to output. */
-        private void print(String aStr)
-        {
-            try { _out.write(aStr); _out.flush(); }
-            catch (IOException e) { throw new RuntimeException(e); }
-        }
+    public void setCancelled(boolean aValue)
+    {
+        if (aValue == _cancelled) return;
+        firePropChange(Cancelled_Prop, _cancelled, _cancelled = aValue);
     }
 
     /**
-     * An TaskMonitor that ignores everything.
+     * Returns whether monitor has finished.
      */
-    TaskMonitor NULL = new TaskMonitor() { };
+    public boolean isFinished()  { return _finished; }
+
+    /**
+     * Sets whether monitor has finished.
+     */
+    public void setFinished(boolean aValue)
+    {
+        if (aValue == _finished) return;
+        firePropChange(Finished_Prop, _finished, _finished = aValue);
+    }
+
+    /**
+     * Returns the task progress.
+     */
+    public double getTaskProgress()  { return _taskDone / (double) _taskTotal; }
+
+    /**
+     * Print string to output.
+     */
+    private void println(String aStr)
+    {
+        try { _writer.write(aStr); _writer.write('\n'); _writer.flush(); }
+        catch (IOException e) { throw new RuntimeException(e); }
+    }
 }
--- src/snap/util/TaskMonitorPanel.java
+++ src/snap/util/TaskMonitorPanel.java
@@ -8,171 +8,161 @@ import snap.viewx.DialogBox;
 /**
  * A TaskMonitor implementation that shows task updates in a panel after a short delay.
  */
-public class TaskMonitorPanel extends ViewOwner implements TaskMonitor {
+public class TaskMonitorPanel extends TaskMonitor {
 
     // The view for progress pane to center on
-    protected View  _view;
+    protected View _view;
 
     // The title for progress pane window to show
-    private String  _title;
+    private String _title;
 
     // The delay before task progress panel appears in milliseconds
-    private int  _delay = 200;
+    private int _delay = 200;
 
-    // The task start time in milliseconds
-    private long  _startTime;
-
-    // The number of tasks completed and the total number of tasks
-    private int  _tasksDone, _tasksTotal;
-
-    // The number of task work units completed and the total number of task work units
-    private int  _taskDone, _taskTotal;
-
-    // The current task title
-    private String  _taskTitle = "First Task";
-
-    // Whether monitor has been cancelled
-    private boolean  _cancelled;
-
-    // The activity label
-    private Label  _activityLabel;
-
-    // The Progress bar
-    private ProgressBar  _progressBar;
-
-    // The dialog box
-    private DialogBox _dialogBox;
-
-    // Whether monitor should print to standard out
-    private boolean  DEBUG_MODE = false;
+    // The ViewOwner for UI
+    private TaskMonitorPanelViewOwner _viewOwner;
 
     /**
      * Constructor for view and title.
      */
     public TaskMonitorPanel(View aView, String aTitle)
     {
+        super();
+        _viewOwner = new TaskMonitorPanelViewOwner();
         _view = aView;
         _title = aTitle;
     }
+
+    /**
+     * Show panel.
+     */
+    protected void showPanel()  { _viewOwner.showDialogBox(); }
+
+    /**
+     * Hide panel.
+     */
+    protected void hide()  { _viewOwner.hideDialogBox(); }
 
     /**
      * Advise the monitor of the total number of subtasks (invoke only once).
      */
     public void startTasks(int aTaskCount)
     {
-        _tasksTotal = aTaskCount;
-        _startTime = System.currentTimeMillis();
-        if (DEBUG_MODE)
-            System.out.println("StartTasks: " + aTaskCount);
+        // Do normal version
+        super.startTasks(aTaskCount);
+
+        // Trigger showPanel after delay
+        ViewUtils.runDelayed(() -> {
+            if (!isFinished())
+                showPanel();
+        }, _delay);
     }
 
     /**
-     * Begin processing a single task.
-     */
-    public void beginTask(String aTitle, int theTotalWork)
-    {
-        _taskTitle = aTitle;
-        _taskTotal = theTotalWork;
-        _taskDone = 0;
-        resetLater();
-        if (DEBUG_MODE)
-            System.out.println("BeginTask " + (_tasksDone + 1) + " of " + _tasksTotal + ": " + aTitle +
-                " (" + theTotalWork + " parts)");
-    }
-
-    /**
-     * Denote that some work units have been completed.
-     */
-    public void updateTask(int theWorkDone)
-    {
-        _taskDone += theWorkDone;
-        resetLater();
-        if (DEBUG_MODE)
-            System.out.println("UpdateTask " + (_tasksDone + 1) + ": " + theWorkDone);
-    }
-
-    /**
-     * Finish the current task, so the next can begin.
-     */
-    public void endTask()
-    {
-        _tasksDone++;
-        _taskDone = _taskTotal;
-        if (_tasksDone >= _taskTotal && _dialogBox != null)
-            runLater(() -> hide());
-        if (DEBUG_MODE)
-            System.out.println("EndTask " + _tasksDone);
-    }
-
-    /**
-     * Check for user task cancellation.
-     */
-    public boolean isCancelled()  { return _cancelled; }
-
-    /**
-     * Show panel.
-     */
-    protected void show()
-    {
-        _dialogBox = new DialogBox();
-        _dialogBox.setTitle(_title);
-        _dialogBox.setContent(getUI());
-        _dialogBox.setOptions("Cancel");
-
-        runLater(() -> _cancelled = _dialogBox.showConfirmDialog(_view));
-    }
-
-    /**
-     * Hide panel.
-     */
-    public void hide()
-    {
-        if (_dialogBox != null)
-            _dialogBox.cancel();
-    }
-
-    /**
-     * Create UI.
+     * Override to hide panel.
      */
     @Override
-    protected View createUI()
+    public void setFinished(boolean aValue)
     {
-        // Create UI
-        Label titleLabel = new Label();
-        titleLabel.setText(_title);
-        _activityLabel = new Label();
-        _progressBar = new ProgressBar();
-        _progressBar.setPrefWidth(360);
-
-        // Create main col view with UI
-        ColView colView = new ColView();
-        colView.setSpacing(8);
-        colView.addChild(titleLabel);
-        colView.addChild(_activityLabel);
-        colView.addChild(_progressBar);
-        return colView;
+        super.setFinished(aValue);
+        if (aValue)
+            hide();
     }
 
     /**
-     * Override
+     * This ViewOwner class shows UI for TaskMonitorPanel.
      */
-    @Override
-    public synchronized void resetLater()
-    {
-        if (_dialogBox == null && System.currentTimeMillis() - _startTime > _delay)
-            show();
-        else if (_dialogBox != null)
-            super.resetLater();
-    }
+    private class TaskMonitorPanelViewOwner extends ViewOwner {
 
-    /**
-     * Reset UI controls.
-     */
-    @Override
-    protected void resetUI()
-    {
-        String countStr = String.format(" (%d of %d)", _tasksDone + 1, _tasksTotal);
-        setViewValue(_activityLabel, _taskTitle + countStr);
-        _progressBar.setProgress(_taskDone / (double) _taskTotal);
+        // The dialog box
+        private DialogBox _dialogBox;
+
+        // The activity label
+        private Label _activityLabel;
+
+        // The Progress bar
+        private ProgressBar _progressBar;
+
+        /**
+         * Constructor.
+         */
+        public TaskMonitorPanelViewOwner()
+        {
+            super();
+        }
+
+        /**
+         * Show panel.
+         */
+        protected void showDialogBox()
+        {
+            _dialogBox = new DialogBox();
+            _dialogBox.setTitle(_title);
+            _dialogBox.setContent(getUI());
+            _dialogBox.setOptions("Cancel");
+
+            // Show dialog box
+            boolean confirmed = _dialogBox.showConfirmDialog(_view);
+            if (!confirmed)
+                setCancelled(true);
+        }
+
+        /**
+         * Hide panel.
+         */
+        public void hideDialogBox()
+        {
+            if (_dialogBox != null)
+                _dialogBox.cancel();
+        }
+
+        /**
+         * Create UI.
+         */
+        @Override
+        protected View createUI()
+        {
+            // Create UI
+            Label titleLabel = new Label();
+            titleLabel.setText(_title);
+            _activityLabel = new Label();
+            _progressBar = new ProgressBar();
+            _progressBar.setPrefWidth(360);
+
+            // Create main col view with UI
+            ColView colView = new ColView();
+            colView.setSpacing(8);
+            colView.addChild(titleLabel);
+            colView.addChild(_activityLabel);
+            colView.addChild(_progressBar);
+            return colView;
+        }
+
+        /**
+         * Initialize when showing.
+         */
+        @Override
+        protected void initShowing()
+        {
+            addPropChangeListener(pc -> resetLater());
+        }
+
+        /**
+         * Reset UI controls.
+         */
+        @Override
+        protected void resetUI()
+        {
+            // Update ActivityLabel
+            String taskTitle = getTaskTitle();
+            int tasksDone = getTasksDone() + 1;
+            int tasksTotal = getTasksTotal();
+            String activityText = String.format("%s (%d of %d)", taskTitle, tasksDone + 1, tasksTotal);
+            setViewValue(_activityLabel, activityText);
+
+            // Update ProgressBar
+            double taskProgress = getTaskProgress();
+            _progressBar.setProgress(taskProgress);
+        }
     }
 }
