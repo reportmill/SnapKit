c4515dc5bd53c812bd6b1b2f44018292438c57bd
Jeff Martin <jeff@reportmill.com>
2024-03-27 12:01:33 -0500

    ClassUtils: Moved some advanced calls to SnapCode

--- src/snap/util/ClassUtils.java
+++ src/snap/util/ClassUtils.java
@@ -58,67 +58,6 @@ public class ClassUtils {
     }
 
     /**
-     * Returns a class for a given name, using the class loader of the given class.
-     */
-    public static Class<?> getClassForName(String aName, ClassLoader aClassLoader)
-    {
-        // Handle arrays, either coded or uncoded (e.g. [I, [D, [LClassName; or  int[], double[] or ClassName[])
-        if (aName.startsWith("["))
-            return getClassForClassCoding(aName, aClassLoader);
-        if (aName.endsWith("[]")) {
-            String cname = aName.substring(0, aName.length() - 2);
-            Class<?> cls = getClassForName(cname, aClassLoader);
-            return cls != null ? Array.newInstance(cls, 0).getClass() : null;
-        }
-
-        // Handle primitive classes
-        Class<?> primitiveClass = getPrimitiveClassForName(aName);
-        if (primitiveClass != null)
-            return primitiveClass;
-
-        // Do normal Class.forName
-        try { return Class.forName(aName, false, aClassLoader); }
-
-        // Handle Exceptions
-        catch(ClassNotFoundException e) { return null; }
-        catch(NoClassDefFoundError t) { System.err.println("ClassUtils.getClass: " + t); return null; }
-        catch(Throwable t) { System.err.println("ClassUtils.getClass: Unknown error: " + t); return null; }
-    }
-
-    /**
-     * Returns a field for a parent class and a name.
-     */
-    public static Field getFieldForName(Class<?> aClass, String aName)
-    {
-        // Get non-primitive class
-        Class<?> cls = aClass.isPrimitive() ? fromPrimitive(aClass) : aClass;
-
-        // Check declared fields
-        Field[] declaredFields = aClass.getDeclaredFields();
-        for (Field field : declaredFields)
-            if (field.getName().equals(aName))
-                return field;
-
-        // Check superclass
-        Class<?> superClass = cls.getSuperclass();
-        if (superClass != null) {
-            Field field = getFieldForName(superClass, aName);
-            if (field != null)
-                return field;
-        }
-
-        // Check interfaces
-        for (Class<?> interf : cls.getInterfaces()) {
-            Field field = getFieldForName(interf, aName);
-            if (field != null)
-                return field;
-        }
-
-        // Return not found
-        return null;
-    }
-
-    /**
      * Class.getMethod wrapper to isolate call to one place.
      */
     public static Method getMethod(Class<?> aClass, String aName, Class<?> ... theClasses)
@@ -141,36 +80,6 @@ public class ClassUtils {
     }
 
     /**
-     * Returns whether name is a primitive class name.
-     */
-    public static boolean isPrimitiveClassName(String aName)
-    {
-        return getPrimitiveClassForName(aName) != null;
-    }
-
-    /**
-     * Returns a primitive class for name.
-     */
-    public static Class<?> getPrimitiveClassForName(String aName)
-    {
-        if (aName.length() > 7 || !Character.isLowerCase(aName.charAt(0)) || aName.indexOf('.') > 0)
-            return null;
-
-        switch (aName) {
-            case "boolean": return boolean.class;
-            case "char": return char.class;
-            case "byte": return byte.class;
-            case "short": return short.class;
-            case "int": return int.class;
-            case "long": return long.class;
-            case "float": return float.class;
-            case "double": return double.class;
-            case "void": return void.class;
-            default: return null;
-        }
-    }
-
-    /**
      * Returns primitive type for non-primitive.
      */
     public static Class<?> toPrimitive(Class<?> aClass)
@@ -190,50 +99,6 @@ public class ClassUtils {
             if (aClass == _primitives[i])
                 return _primMappings[i];
         return aClass;
-    }
-
-    /**
-     * Returns a class for given class coding.
-     */
-    public static Class<?> getClassForClassCoding(String aName, ClassLoader aClassLoader)
-    {
-        char c = aName.charAt(0);
-        switch (c) {
-            case 'B': return byte.class;
-            case 'C': return char.class;
-            case 'D': return double.class;
-            case 'F': return float.class;
-            case 'I': return int.class;
-            case 'J': return long.class;
-            case 'S': return short.class;
-            case 'Z': return boolean.class;
-            case 'V': return void.class;
-            case 'L':
-                int end = aName.indexOf(';', 1);
-                return getClassForName(aName.substring(1, end), aClassLoader);
-            case '[':
-                Class<?> cls = getClassForClassCoding(aName.substring(1), aClassLoader);
-                return cls != null ? Array.newInstance(cls, 0).getClass() : null;
-        }
-        throw new RuntimeException("ClassUtils.getCodedPrimitiveClass: Not a coded class " + aName);
-    }
-
-    /**
-     * Returns a class coding for given class.
-     */
-    public static String getClassCodingForClass(Class<?> aClass)
-    {
-        if (aClass.isArray()) return "[" + getClassCodingForClass(aClass.getComponentType());
-        if (aClass == byte.class) return "B";
-        if (aClass == char.class) return "C";
-        if (aClass == double.class) return "D";
-        if (aClass == float.class) return "F";
-        if (aClass == int.class) return "I";
-        if (aClass == long.class) return "J";
-        if (aClass == short.class) return "S";
-        if (aClass == boolean.class) return "Z";
-        if (aClass == void.class) return "V";
-        return "L" + aClass.getName() + ";";
     }
 
     /**
