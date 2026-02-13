128f4b4cd03dfef482f2187c0b4706c5f634afa1
Smithor <gamedud75@hotmail.com>
2026-02-13 11:32:50 -0700

    Documentation change.

--- src/snap/view/AbstractViewOwner.java
+++ src/snap/view/AbstractViewOwner.java
@@ -4,32 +4,28 @@ public abstract class AbstractViewOwner extends ViewOwner {
 
     /**
      * Creates the top level view for this owner.
-     * <br> <br>
-     * Implementation note: If you want the vanilla interaction of this class, return super.createUI();
+     * @apiNote If you want the vanilla interaction of this class, return {@code super.createUI();}
      */
     @Override
     abstract protected View createUI();
 
     /**
      * Initializes the UI panel.
-     * <br> <br>
-     * Implementation note: If you want the vanilla interaction of this class, return super.initUI();
+     * @apiNote If you want the vanilla interaction of this class, return {@code super.initUI();}
      */
     @Override
     abstract protected void initUI();
 
     /**
      * Reset UI controls.
-     * <br> <br>
-     * Implementation note: If you want the vanilla interaction of this class, return super.resetUI();
+     * @apiNote If you want the vanilla interaction of this class, return {@code super.resetUI();}
      */
     @Override
     abstract protected void resetUI();
 
     /**
      * Respond to UI controls.
-     * <br> <br>
-     * Implementation note: If you want the vanilla interaction of this class, return super.respondUI(ViewEvent);
+     * @apiNote If you want the vanilla interaction of this class, return {@code super.respondUI(ViewEvent);}
      * @param anEvent Any event that has been caught by this ViewOwner.
      */
     @Override
