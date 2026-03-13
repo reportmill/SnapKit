606aaccb6fb62adff17ed9001e00a005958038ff
Smithor <gamedud75@hotmail.com>
2026-03-13 15:31:30 -0600

    Added deprecation notice on ViewOwner.

--- src/snap/view/ViewOwner.java
+++ src/snap/view/ViewOwner.java
@@ -2,7 +2,9 @@ package snap.view;
 
 /**
  * Temporary support for legacy ViewOwner.
+ * @deprecated see {@link ViewController}
  */
+@Deprecated(since="2026.03")
 public class ViewOwner extends ViewController {
 
 }
