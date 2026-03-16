4467c1ccd6453f7aed5b71f44c457cb3084c90a6
Smithor <gamedud75@hotmail.com>
2026-03-16 09:26:19 -0600

    SimpleViewController Documentation

--- src/snap/view/SimpleViewController.java
+++ src/snap/view/SimpleViewController.java
@@ -1,7 +1,13 @@
 package snap.view;
 
 /**
- * A simple implementation of ViewController that has most functionality already implemented
+ * A simple implementation of ViewController that has most functionality already implemented. This implementation will
+ * load the UI from a .snp file named the same as this class, located in the resources dir of the jar package. The UI
+ * can be managed by implementing the {@link #initUI()} method, and referencing the names of components inside of that
+ * loaded View. To add events or feedback to elements of the UI, implement the {@link #respondUI(ViewEvent)} method and
+ * add your logic there.
+ * <br>
+ * All abstract methods of this class can be implemented or left empty depending on your needs.
  */
 public abstract class SimpleViewController extends ViewController {
 
